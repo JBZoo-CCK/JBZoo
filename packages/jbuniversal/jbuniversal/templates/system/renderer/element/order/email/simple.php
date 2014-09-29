b396b48ac9b5fb901d31645fd74646cd6168c7e1
Alex <t_tapak@yahoo.com>
2014-09-29 12:02:35 +0000

    + добавлен пропущенный стиль для jbprice + Перенес стили из catalog в system для email'a order'a

---
+++ packages/jbuniversal/jbuniversal/templates/system/renderer/element/order/email/simple.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$params = $this->app->data->create($params);
+$label  = '';
+if ($params->get('showlabel')) {
+
+    // check label
+    $labelText = $element->config->get('name');
+
+    $label = '<strong>' . $labelText . '</strong>';
+
+}
+
+// render HTML for current element
+$render = $element->render($params);
+
+// render result
+echo $label . $render . "\n";
