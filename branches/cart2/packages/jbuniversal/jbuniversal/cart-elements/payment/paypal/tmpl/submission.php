96ce8f24df07984a2163d6419772ad3c2c611d9d
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2014-10-14 13:30:39 +0000

    * удалены старые шаблоны систем оплаты

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/payment/paypal/tmpl/submission.php
@@ -0,0 +1,6 @@
+
+<!--noindex-->
+<a href="http://paypal.com/" target="_blank" rel="nofollow">
+    <img src="<?php echo JUri::root(); ?>media/zoo/applications/jbuniversal/cart-elements/payment/paypal/assets/logo.png">
+</a>
+<!--/noindex-->
