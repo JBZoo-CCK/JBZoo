92f8ab15977d8f9b480021c55c53403e52c3aa81
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2014-10-14 13:42:45 +0000

    Страница заказа в панели управления, мелкие правки после обновления UIkit

--- branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jborder/_edit_orderinfo.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jborder/_edit_orderinfo.php
@@ -4,7 +4,7 @@
 ?>
 
 <div class="uk-panel">
-    <h2 class="uk-panel-title">Информация о заказе от покупателя</h2>
+    <h2>Информация о заказе от покупателя</h2>
 
     <dl class="uk-description-list-horizontal">
         <?php echo $this->orderFieldRender->renderAdminEdit(array('order' => $order)); ?>
