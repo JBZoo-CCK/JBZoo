c1278eebdd5cf838890edaf2ada7bfaa6f7ebd67
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2014-10-20 08:40:08 +0000

    Шаблон с радио кнопками для bool

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/price/bool/tmpl/radio.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$data = array(
+    ''                    => ' - ' . JText::_('JBZOO_CORE_PRICE_OPTIONS_DEFAULT') . ' - ',
+    JText::_('JBZOO_NO')  => JText::_('JBZOO_NO'),
+    JText::_('JBZOO_YES') => JText::_('JBZOO_YES')
+);
+
+?>
+
+<div class="controls">
+    <?php echo $this->app->jbhtml->radio($data, $this->getControlName('value'), null, $this->getValue('value')); ?>
+</div>
+
+
+
+
