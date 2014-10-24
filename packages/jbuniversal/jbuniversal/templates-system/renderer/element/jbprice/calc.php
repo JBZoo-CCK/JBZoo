859e7b7279133a0ef52aebb04e3106ef4dc33656
Denis Smetannikov <denis@jbzoo.com>
2014-10-24 10:57:06 +0000

    тултип у калькулятора

--- packages/jbuniversal/jbuniversal/templates-system/renderer/element/jbprice/calc.php
+++ packages/jbuniversal/jbuniversal/templates-system/renderer/element/jbprice/calc.php
@@ -16,13 +16,19 @@ defined('_JEXEC') or die('Restricted access');
 $label = ($params['altlabel']) ? $params['altlabel'] : $element->config->get('name');
 $desc  = $element->getDescription();
 
+$this->app->jbassets->initTooltip();
+
 ?>
 
 <tr class="element-<?php echo $element->getType(); ?>">
 
     <td class="clac-label">
-        <label class="hasTip" title="<?php echo $desc; ?>"><?php echo $label; ?></label>
-        <em><?php echo $desc; ?></em>
+        <label>
+            <?php echo $label; ?>
+            <?php if ($desc) : ?>
+                <span class="jbtooltip" title="1<?php echo $desc; ?>"> </span>
+            <?php endif; ?>
+        </label>
     </td>
 
     <td class="clac-field">
