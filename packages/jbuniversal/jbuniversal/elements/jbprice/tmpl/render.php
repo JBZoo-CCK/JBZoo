82c30486c1567f5298b8203acd2ddee0caecf545
Alex <t_tapak@yahoo.com>
2014-11-25 01:21:50 +0000

    + JBPrice, JBPricePlain, JBPriceCalc + JBVariantList, JBVariant

---
+++ packages/jbuniversal/jbuniversal/elements/jbprice/tmpl/render.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$class  = 'jsJBPriceAdvance-' . $this->identifier . '-' . $this->getItem()->id;
+$unique = $this->app->jbstring->getId('jbprice-adv-'); ?>
+<div class="jsJBPriceAdvance jbprice-advance <?php echo $class; ?>" id="<?php echo $unique; ?>">
+    <?php echo $data; ?>
+</div>
+
+<script type="text/javascript">
+    (function ($) {
+
+        $('#<?php echo $unique;?>').JBZooPrice({
+            'elements': <?php echo json_encode($elements);?>,
+
+            'itemId'    : <?php echo $this->getItem()->id;?>,
+            'identifier': "<?php echo $this->identifier;?>",
+
+            'isInCart'  : <?php echo $isInCart;?>,
+            'variantUrl': "<?php echo $variantUrl; ?>"
+        });
+
+    })(jQuery);
+</script>
---
+++ packages/jbuniversal/jbuniversal/elements/jbpriceplain/jbpriceplain.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+App::getInstance('zoo')->loader->register('ElementJBPrice', 'elements:jbprice/jbprice.php');
+
+/**
+ * Class ElementJBPricePlain
+ * The Price element for JBZoo
+ */
+class ElementJBPricePlain extends ElementJBPrice implements iSubmittable
+{
+    /**
+     * Get variant from $this->data() by values
+     * MODE: DEFAULT
+     *
+     * @param  array $values values from front end
+     *
+     * @return array
+     */
+    public function getVariantByValues($values = array())
+    {
+        $data = $this->get('values', array());
+
+        if (empty($values) || empty($data)) {
+            return $values;
+        }
+
+        $variations = $this->get('variations', array());
+        unset($variations[self::BASIC_VARIANT]);
+        foreach ($data as $i => $value) {
+            foreach ($values as $identifier => $fields) {
+
+                $valError = false;
+                $idError  = false;
+
+                if (!isset($value[$identifier]) ||
+                    count($values) !== count($value)
+                ) {
+                    $idError = true;
+                }
+
+                if ($idError === false) {
+                    if (isset($fields['value']) && (JString::strlen($fields['value']) === 0)) {
+                        unset($fields);
+                    }
+
+                    if (!empty($fields)) {
+                        $diff = array_diff_assoc($fields, $value[$identifier]);
+                    }
+
+                    if (!empty($diff)) {
+                        $valError = true;
+                    }
+                }
+
+                if ($idError === true || $valError === true) {
+                    unset($variations[$i]);
+                }
+            }
+        }
+
+        return $variations;
+    }
+
+    /**
+     * @param string $template - need to get render params for elements
+     * @param array  $values - selected values
+     */
+    public function ajaxChangeVariant($template = 'default', $values = array())
+    {
+        $list = $this->getVariantByValues($values);
+
+        $key = (JString::strlen(key($list)) > 0 ? key($list) : self::BASIC_VARIANT);
+
+        $this->set('default_variant', $key);
+
+        $this->_template = $template;
+        $this->_list     = new JBCartVariantList($list, $this);
+
+        $this->app->jbajax->send($this->_list->renderVariant());
+    }
+
+    /**
+     * Ajax add to cart method
+     *
+     * @param string $template
+     * @param int    $quantity
+     * @param array  $values
+     * @param bool   $sendAjax
+     */
+    public function ajaxAddToCart($template = 'default', $quantity = 1, $values = array(), $sendAjax = true)
+    {
+        $jbAjax = $this->app->jbajax;
+
+        $cart = JBCart::getInstance();
+        $list = $this->getVariantByValues($values);
+        $key  = (JString::strlen(key($list)) > 0 ? key($list) : self::BASIC_VARIANT);
+
+        $this->set('default_variant', $key);
+
+        $this->_template = $template;
+        $this->_list     = new JBCartVariantList($list, $this, array(
+            'values'   => $values,
+            'quantity' => $quantity
+        ));
+
+        if ($this->inStock($quantity)) {
+
+            $cart->addItem($this->_list->getCartData());
+
+            $sendAjax && $jbAjax->send(array(), true);
+
+        } else {
+
+            $sendAjax && $jbAjax->send(array('message' => JText::_('JBZOO_JBPRICE_ITEM_NO_QUANTITY')), false);
+        }
+
+        $sendAjax && $jbAjax->send(array('added' => 0, 'message' => JText::_('JBZOO_JBPRICE_NOT_AVAILABLE_MESSAGE')));
+    }
+
+    /**
+     * @param $identifier
+     *
+     * @return array|void
+     */
+    public function elementOptions($identifier)
+    {
+        return parent::selectedOptions($identifier);
+    }
+
+}
