54e1588a2d8487ae583c1a3598560ae4d3032ad5
Denis Smetannikov <denis@jbzoo.com>
2014-11-30 06:35:44 +0000

    goto widget

---
+++ packages/jbuniversal/jbuniversal/assets/js/widget/goto.js
@@ -0,0 +1,31 @@
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ */
+
+;
+(function ($, window, document, undefined) {
+
+
+    JBZoo.widget('JBZoo.Goto', {}, {
+
+        'click {element}': function (e, $this) {
+
+            var url = $(this).attr('href');
+
+            if (!url) {
+                url = $(this).data('href');
+            }
+
+            parent.location.href = url;
+            return false;
+        }
+    });
+
+
+})(jQuery, window, document);
