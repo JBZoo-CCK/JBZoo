92a0dd66dba86af6de3b8c066541467011e5d606
Denis Smetannikov <denis@jbzoo.com>
2014-11-30 06:37:32 +0000

    триггер, деструктор, колбеки создания и удаления виджета

--- packages/jbuniversal/jbuniversal/assets/js/widget.js
+++ packages/jbuniversal/jbuniversal/assets/js/widget.js
@@ -18,21 +18,6 @@
     $.extend(JBZoo.constructor.prototype, {
 
         /**
-         * Prototype class extending
-         * @param Child
-         * @param Parent
-         */
-        classExtend: function (Child, Parent) {
-            var JBZooObject = function () {
-            };
-
-            $.extend(JBZooObject.prototype, Child.prototype, Parent.prototype);
-            Child.prototype = new JBZooObject;
-            Child.prototype.constructor = Child;
-            Child.parent = Parent.prototype
-        },
-
-        /**
          * Widget creator
          * @param widgetName
          * @param defaults
@@ -70,22 +55,32 @@
             }
 
             widgets[widgetName] = function (element, options) {
+                var $this = this;
+
                 // system
                 instanceId++;
-                this._id = instanceId;
-                this._name = widgetName;
+                $this._id = instanceId;
+                $this._name = widgetName;
 
                 // for widget
-                this.el = $(element);
-                this.options = $.extend(true, {}, this._defaults, options);
+                $this.el = $(element);
+                $this.options = $.extend(true, {}, $this._defaults, options);
 
                 // init
-                this.init(this);
-                this._initEvents(this._eventList, this);
+                $this._initEvents(this._eventList, this);
+                $this.init(this);
+
+                // onCreate event
+                if ($.isFunction(this.options.onCreate)) {
+                    $this.options.onCreate.apply($this);
+                }
             };
 
             // widget extending
-            var extendedDefaultList = {},
+            var extendedDefaultList = {
+                    onCreate : $.noop,
+                    onDestroy: $.noop
+                },
                 extendedEventList = {},
                 lastParent = '';
 
@@ -158,9 +153,14 @@
                      * System destructor
                      */
                     destroy: function () {
-                        this._destroy.apply(this, arguments);
-                        this.off('');
-                        this.el.removeData(this._name);
+                        var $this = this;
+
+                        $this._destroy.apply(this, arguments);
+                        $this.off('');
+                        $this.el.removeData(this._name);
+                        if ($.isFunction(this.options.onCreate)) {
+                            this.options.onDestroy.apply($this);
+                        }
                     },
 
                     /**
@@ -182,6 +182,10 @@
                      * @returns jQuery
                      */
                     $: function (selector) {
+                        if (selector == '{element}') {
+                            return this.el;
+                        }
+
                         return $(selector, this.el);
                     },
 
@@ -229,6 +233,19 @@
                     },
 
                     /**
+                     * Manual event calling
+                     * @param trigger
+                     * @param selector
+                     * @param data
+                     * @private
+                     */
+                    _trigger: function (trigger, selector, data) {
+                        data = data || {};
+                        selector = selector || '{element}';
+                        this.$(selector).trigger(trigger + '.' + this._name, data);
+                    },
+
+                    /**
                      * @param handler
                      * @param delay
                      * @returns {number}
@@ -271,6 +288,7 @@
 
             // plugin initialize (HANDS OFF!!!)
             $.fn[widgetName] = function (options) {
+
                 var args = arguments,
                     method = (args[0] && typeof args[0] == 'string') ? args[0] : null;
 
@@ -300,6 +318,21 @@
                 // chain jQuery functions
                 return $(this);
             };
+        },
+
+        /**
+         * Prototype class extending
+         * @param Child
+         * @param Parent
+         */
+        classExtend: function (Child, Parent) {
+            var JBZooObject = function () {
+            };
+
+            $.extend(JBZooObject.prototype, Child.prototype, Parent.prototype);
+            Child.prototype = new JBZooObject;
+            Child.prototype.constructor = Child;
+            Child.parent = Parent.prototype
         }
     });
 
