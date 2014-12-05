4e2ec0c0835f0849b568e34863b3178a15360909
Sergey <kalistratov.s.m@gmail.com>
2014-12-05 14:45:22 +0000

    # ADD: Payment LiqPay element

---
+++ packages/jbuniversal/jbuniversal/cart-elements/payment/liqpay/tmpl/submission.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Kalistratov Sergey <kalistratov.s.m@gmail.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+?>
+
+<!--noindex-->
+<a href="https://www.liqpay.com/ru" target="_blank" rel="nofollow">
+    <img src="<?php echo JUri::root(); ?>media/zoo/applications/jbuniversal/cart-elements/payment/liqpay/assets/logo.png">
+</a>
+<!--/noindex-->
