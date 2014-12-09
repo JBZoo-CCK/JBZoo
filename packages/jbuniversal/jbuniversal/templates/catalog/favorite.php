a14334434ea85f8a20e0c37b7ae8ed0b1e82a725
Denis Smetannikov <denis@jbzoo.com>
2014-12-09 15:15:06 +0000

    Новое избранное

--- packages/jbuniversal/jbuniversal/templates/catalog/favorite.php
+++ packages/jbuniversal/jbuniversal/templates/catalog/favorite.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
- *
  * @package     jbzoo
  * @version     2.x Pro
  * @author      JBZoo App http://jbzoo.com
@@ -18,31 +17,40 @@ $this->app->jbdebug->mark('template::favorite::start');
 $this->app->jblayout->setView($this);
 $this->app->document->setTitle(JText::_('JBZOO_FAVORITE_ITEMS'));
 $this->app->jbwrapper->start();
-
 $this->app->jbassets->favorite();
 
-?><h1 class="title"><?php echo JText::_('JBZOO_FAVORITE_ITEMS'); ?></h1><?php
+?>
+    <h1 class="title"><?php echo JText::_('JBZOO_FAVORITE_ITEMS'); ?></h1>
 
-if (!empty($this->items)) {
-    // items
-    ?>
-    <a class="jsFavoriteClear" href="<?php echo $this->app->jbrouter->favoriteClear(); ?>">Удалить избрнное</a>
-    <script>
+    <div class="jbfavorite-wrapper jsFavoriteList">
+        <?php
+        if (!empty($this->items)) {
+            ?>
+
+            <div class="clearfix">
+                <span class="jsFavoriteClear jbfavorite-remove-all jbbutton orange">
+                    <?php echo JText::_('JBZOO_FAVORITE_REMOVE_ALL'); ?>
+                </span>
+            </div>
+
+            <?php
+            echo $this->app->jblayout->render('favorite', $this->items);
+            echo '<p class="jsJBZooFavoriteEmpty jbfavorite-empty" style="display:none;">'
+                . JText::_('JBZOO_FAVORITE_ITEMS_NOT_FOUND') . '</p>';
+
+        } else {
+            echo '<p class="jbfavorite-empty">' . JText::_('JBZOO_FAVORITE_ITEMS_NOT_FOUND') . '</p>';
+        }
+        ?>
+    </div>
+
+    <script type="text/javascript">
         jQuery(function ($) {
-            $('.jsFavoriteClear').click(function () {
-                $.post($(this).attr('href'), {}, function () {
-                    window.location.reload();
-                }, 'json');
-                return false;
-            });
+            $('.jsFavoriteList').JBZooFavoriteList(<?php echo json_encode(array(
+                'url_clear' => $this->app->jbrouter->favoriteClear()
+            ));?>);
         });
     </script>
-    <?php
-    echo $this->app->jblayout->render('favorite', $this->items);
-    echo '<p class="jsJBZooFavoriteEmpty" style="display:none;">' . JText::_('JBZOO_FAVORITE_ITEMS_NOT_FOUND') . '</p>';
-} else {
-    echo '<p>' . JText::_('JBZOO_FAVORITE_ITEMS_NOT_FOUND') . '</p>';
-}
-
+<?php
 $this->app->jbwrapper->end();
 $this->app->jbdebug->mark('template::favorite::finish');
