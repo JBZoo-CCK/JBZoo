964a208480d4243905b5b4520d09207665ec2934
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2014-12-19 10:16:13 +0000

    норм шаблоны для доставок

--- branches/cart2/packages/jbuniversal/jbuniversal/templates-system/renderer/element/order/shipping.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates-system/renderer/element/order/shipping.php
@@ -24,8 +24,10 @@ if ($isError) {
 
 // create class attribute
 $classes = array_filter(array(
+    'jsShipping',
     'jbcart-shipping-row',
     'jbcart-shipping-' . $element->getElementType(),
+    $element->isDefault() ? 'active' : '',
     $params->get('first') ? 'first' : '',
     $params->get('last') ? 'last' : '',
     $isError ? 'error' : '',
@@ -37,34 +39,58 @@ $uniqId = 'jbcart-' . $element->identifier;
 
 ?>
 
-<div class="<?php echo implode(' ', $classes); ?>">
+<div class="<?php echo implode(' ', $classes); ?>" data-type="<?php echo $element->getElementType(); ?>">
 
     <input <?php echo $this->app->jbhtml->buildAttrs(array(
         'type'    => 'radio',
         'id'      => $uniqId,
         'value'   => $element->identifier,
-        'class'   => 'jbcart-radio-input jbcart-shipping-radio',
+        'class'   => array(
+            'jsRadio',
+            'jbcart-radio-input',
+            'jbcart-shipping-radio',
+        ),
         'name'    => $element->getControlName('_shipping_id'),
         'checked' => $element->isDefault() ? 'checked' : '',
     ));?> />
 
-    <label class="jbcart-radio-label jbcart-shipping-label" for="<?php echo $uniqId; ?>">
+    <div class="jbcart-radio-label jbcart-shipping-wrapper">
 
         <div class="jbcart-radio"></div>
 
-        <div class="jbcart-shipping-name">
+        <label class="jbcart-shipping-name" for="<?php echo $uniqId; ?>">
             <?php echo $label; ?>
-            <span class="jbcart-shipping-price jsShippingPrice">
-                 (<?php echo $element->getRate(); ?> )
+            <span class="jbcart-shipping-price jsShippingElementPrice">
+                (<span class="jsPrice-<?php echo $element->identifier; ?>">
+                    <?php echo $element->getRate()->html(); ?>
+                </span>)
             </span>
-        </div>
-        <div class="jbcart-shipping-element"><?php echo $element->renderSubmission($params); ?></div>
+        </label>
 
-        <?php if ($description = $element->config->get('description')) : ?>
-            <p class="jbcart-shipping-desc"><?php echo JText::_($description); ?> </p>
-        <?php endif; ?>
+        <?php
+        $description = $element->config->get('description');
+        $element     = $element->renderSubmission($params);
+
+        if ($element || $description) {
+
+            $html = array();
+
+            $html[] = '<div class="jbcart-shipping-element jsShippingElement">';
+
+            if ($description) {
+                $html[] = '<p class="jbcart-shipping-desc">' . JText::_($description) . '</p>';
+            }
+
+            if ($element) {
+                $html[] = $element;
+            }
+
+            $html[] = '</div>';
+
+            echo implode("\n", $html);
+        } ?>
 
         <?php echo $error; ?>
-    </label>
+    </div>
 
 </div>
