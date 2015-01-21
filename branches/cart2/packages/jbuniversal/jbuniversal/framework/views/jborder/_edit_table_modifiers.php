3a94fd2139c628f0bc9d34a56ccf7fa748c2b2ee
tapakan <tapakan@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-01-21 18:52:50 +0000

    * Системые шаблоны параметров цены для админки * Таблица ордера в админке

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jborder/_edit_table_modifiers.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$i = 0;
+foreach ($modifiers as $modifier) :
+    $i++;
+    $this->sum->addModify($modifier);
+    if ($i == 1) : ?>
+        <tr>
+            <td rowspan="<?php echo count($modifiers); ?>" class="noborder-btm"></td>
+            <td rowspan="<?php echo count($modifiers); ?>">
+                <strong><?php echo JText::_('JBZOO_ORDER_MODIFIERS_OTHER'); ?></strong><br>
+                <i>(<?php echo JText::_('JBZOO_ORDER_MODIFIERS_OTHER_ELEMENTS'); ?>)</i>
+            </td>
+            <td>
+                <?php echo $modifier->getName(); ?>
+            </td>
+            <td class="align-right">
+                <?php echo $modifier->getRate()->htmlAdv($currency, true); ?>
+            </td>
+            <td class="align-right">
+                <?php echo $this->sum->html(); ?>
+            </td>
+        </tr>
+    <?php else : ?>
+        <tr>
+            <td><?php echo $modifier->getName(); ?></td>
+            <td class="align-right">
+                <?php echo $modifier->getRate()->htmlAdv($currency, true); ?>
+            </td>
+            <td class="align-right">
+                <?php echo $this->sum->html(); ?>
+            </td>
+        </tr>
+    <?php endif;
+endforeach;
---
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jborder/_edit_table_payment.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$this->sum->addModify($payment); ?>
+<tr>
+    <td class="noborder-btm"></td>
+    <th>
+        <p><?php echo JText::_('JBZOO_ORDER_PAYMENT_FEE'); ?></p>
+    </th>
+    <td>
+        <?php echo $payment->getName(); ?>
+    </td>
+    <td class="align-right">
+        <?php echo $payment->getRate()->htmlAdv($currency, true); ?>
+    </td>
+    <td class="align-right">
+        <?php echo $this->sum->html(); ?>
+    </td>
+</tr>
