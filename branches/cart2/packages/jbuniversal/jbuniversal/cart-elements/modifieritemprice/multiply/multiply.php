df30515a24a48c6a092b126b22445552c40b4d3e
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-01-28 09:54:12 +0000

    рефакторинг модификаторов цкны товара

--- branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/modifieritemprice/multiply/multiply.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/modifieritemprice/multiply/multiply.php
@@ -18,49 +18,17 @@ defined('_JEXEC') or die('Restricted access');
 class JBCartElementModifierItemPriceMultiply extends JBCartElementModifierItemPrice
 {
     /**
-     * /**
-     * @param \JBCartValue $value
-     * @param  array       $data
-     * @return mixed
+     * @return JBCartValue
      */
-    public function edit(JBCartValue &$value, $data = array())
+    public function getRate()
     {
-        if ($layout = $this->getLayout('edit.php')) {
-            $this->modify($value);
+        if ($this->_isValid()) {
+            $rate       = $this->config->get('rate', 0);
+            $multiplier = $this->app->jbvars->number($rate) * 100;
 
-            return self::renderLayout($layout, array(
-                'rate'  => $this->_order->val($this->get('rate', 0)),
-                'value' => $value
-            ));
+            return $this->_order->val($multiplier, '%');
         }
 
-        return null;
-    }
-
-    /**
-     * @param JBCartValue    $value
-     * @param ElementJBPrice $jbPrice
-     * @param array          $session_data
-     * @return \JBCartValue
-     */
-    public function modify(JBCartValue $value, $jbPrice = null, $session_data = null)
-    {
-        $rate = (float)$this->getRate($jbPrice, $session_data)->val();
-
-        return $value->multiply($rate);
-    }
-
-    /**
-     * @param ElementJBPrice $jbPrice
-     * @param array          $session_data
-     * @return \JBCartValue
-     */
-    public function getRate($jbPrice = null, $session_data = null)
-    {
-        if ($this->_isValid($jbPrice->getItem())) {
-            return $this->_order->val($this->config->get('value'));
-        }
-
-        return $this->_order->val();
+        return $this->_order->val(0, '%');
     }
 }
