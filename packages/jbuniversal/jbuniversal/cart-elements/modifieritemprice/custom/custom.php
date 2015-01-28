87c5e4c8f00cb13d3a800fcbe0b1c0527fbe927c
Denis Smetannikov <denis@jbzoo.com>
2015-01-28 09:54:12 +0000

    рефакторинг модификаторов цкны товара

--- packages/jbuniversal/jbuniversal/cart-elements/modifieritemprice/custom/custom.php
+++ packages/jbuniversal/jbuniversal/cart-elements/modifieritemprice/custom/custom.php
@@ -17,50 +17,5 @@ defined('_JEXEC') or die('Restricted access');
  */
 class JBCartElementModifierItemPriceCustom extends JBCartElementModifierItemPrice
 {
-    /**
-     * @param \JBCartValue $value
-     * @param  JSONData|array    $data
-     * @return mixed
-     */
-    public function edit(JBCartValue &$value, $data = array())
-    {
-        if ($layout = $this->getLayout('edit.php')) {
-            $rate = $this->_order->val($this->get('rate', 0));
-            $value->add($rate);
 
-            return self::renderLayout($layout, array(
-                'rate'  => $rate,
-                'value' => $value
-            ));
-        }
-
-        return null;
-    }
-
-    /**
-     * @param JBCartValue    $value
-     * @param ElementJBPrice $jbPrice
-     * @param array          $session_data
-     * @return \JBCartValue
-     */
-    public function modify(JBCartValue $value, $jbPrice = null, $session_data = null)
-    {
-        $rate = $this->getRate($jbPrice, $session_data);
-
-        return $value->add($rate);
-    }
-
-    /**
-     * @param ElementJBPrice $jbPrice
-     * @param array          $session_data
-     * @return \JBCartValue
-     */
-    public function getRate($jbPrice = null, $session_data = null)
-    {
-        if ($this->_isValid($jbPrice->getItem())) {
-            return $this->_order->val($this->config->get('value'));
-        }
-
-        return $this->_order->val();
-    }
 }
--- packages/jbuniversal/jbuniversal/cart-elements/modifieritemprice/quantity/quantity.php
+++ packages/jbuniversal/jbuniversal/cart-elements/modifieritemprice/quantity/quantity.php
@@ -18,56 +18,26 @@ defined('_JEXEC') or die('Restricted access');
 class JBCartElementModifierItemPriceQuantity extends JBCartElementModifierItemPrice
 {
     /**
-     * @param \JBCartValue $value
-     * @param  JSONData|array    $data
-     * @return mixed
+     * @return JBCartValue
      */
-    public function edit(JBCartValue &$value, $data = array())
+    public function getRate()
     {
-        if ($layout = $this->getLayout('edit.php')) {
-            $rate = $this->_order->val($this->get('rate', 0));
-            $value->add($rate);
+        $isAjax   = $this->app->jbrequest->isAjax();
+        $isValid  = $this->_isValid();
+        $jbPrice  = $this->_price;
+        $itemData = $this->_itemData;
+        $quantity = (float)$this->config->get('quantity', 0);
 
-            return self::renderLayout($layout, array(
-                'rate'  => $rate,
-                'value' => $value
-            ));
-        }
-
-        return null;
-    }
-
-    /**
-     * @param JBCartValue    $value
-     * @param ElementJBPrice $jbPrice
-     * @param array          $session_data
-     * @return \JBCartValue
-     */
-    public function modify(JBCartValue $value, $jbPrice = null, $session_data = null)
-    {
-        $rate = $this->getRate($jbPrice, $session_data);
-
-        return $value->add($rate);
-    }
-
-    /**
-     * @param ElementJBPrice $jbPrice
-     * @param array          $session_data
-     * @return \JBCartValue
-     */
-    public function getRate($jbPrice = null, $session_data = null)
-    {
-        if ($this->app->jbrequest->isAjax() && $this->_isValid($jbPrice->getItem())) {
-            $quantity = (float)$this->config->get('quantity', 0);
+        if ($isAjax && $isValid) {
 
             $list = $jbPrice->getVariantList();
-            if (($session_data && $session_data['quantity'] >= $quantity) ||
-                (!$session_data && $list->quantity >= $quantity)
+            if (($itemData && $itemData->get('quantity', 1) >= $quantity) ||
+                (!$itemData && $list->quantity >= $quantity)
             ) {
-                return $this->_order->val($this->config->get('value'));
+                return $this->_order->val($this->config->get('rate'));
             }
         }
 
-        return $this->_order->val();
+        return $this->_order->val(0);
     }
 }
