362cdbc6569b5c1149f1fcf05b169205f0a754d6
Alex <t_tapak@yahoo.com>
2015-01-28 11:22:55 +0000

    + Шаблоны для shippingfields

---
+++ packages/jbuniversal/jbuniversal/cart-elements/shippingfield/text/tmpl/submission.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$attrs = array(
+    'type' => 'text',
+    'name' => $this->getControlName('value'),
+    'id'   => $this->htmlId(true)
+); ?>
+    <input <?php echo $this->app->jbhtml->buildAttrs($attrs); ?> />
+
+<?php if ($description = $this->config->get('description')) : ?>
+    <p class="shippingfileds-description"> <?php echo $description; ?> </p>
+<?php endif;
---
+++ packages/jbuniversal/jbuniversal/cart-elements/shippingfield/textarea/tmpl/submission.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$attrs = array(
+    'name' => $this->getControlName('value'),
+    'id'   => $this->htmlId(true)
+);
+
+?>
+    <textarea <?php echo $this->app->jbhtml->buildAttrs($attrs); ?>></textarea>
+
+<?php if ($description = $this->config->get('description')) : ?>
+    <p class="shippingfileds-description"> <?php echo $description; ?> </p>
+<?php endif;
