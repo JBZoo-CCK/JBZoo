63339e4df96b1df87b94caf0fbdb05a517648436
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-01-29 14:02:14 +0000

    Более подходящий шаблон письма для мобилок

--- branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/email/items/tmpl/order.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/email/items/tmpl/order.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
- *
  * @package     jbzoo
  * @version     2.x Pro
  * @author      JBZoo App http://jbzoo.com
@@ -31,7 +30,7 @@ if (!empty($items)) : ?>
         )) .
         $this->getStyles(array(
             'border-collapse' => 'collapse'
-        ));?>
+        )); ?>
         >
 
         <!-- table_head.php -->
--- branches/cart2/packages/jbuniversal/jbuniversal/templates-system/renderer/email/order.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates-system/renderer/email/order.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
- *
  * @package     jbzoo
  * @version     2.x Pro
  * @author      JBZoo App http://jbzoo.com
@@ -15,65 +14,35 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 <table <?php echo $this->getAttrs(array(
         'cellpadding' => 0,
-        'width'       => '100%'
+        'width'       => '800px;'
     )) .
     $this->getStyles(array(
         'border-collapse' => 'collapse',
         'font-size'       => '14px'
-    ));?>
+    )); ?>
     >
+
     <?php if ($this->checkPosition('title')) : ?>
         <tr>
-            <td>
-                <h1>
-                    <?php echo $this->renderPosition('title'); ?>
-                </h1>
-            </td>
+            <td><h1><?php echo $this->renderPosition('title'); ?></h1></td>
         </tr>
     <?php endif; ?>
+
     <tr>
         <td <?php echo $this->getAttrs(array(
-                'width'  => '65%',
-                'height' => '50%',
                 'valign' => 'top'
             )) .
             $this->getStyles(array(
                 'padding' => '0 40px 0 0'
             )); ?>
             >
-            <?php if ($this->checkPosition('items')) :
-                echo $this->renderPosition('items');
-            endif; ?>
-        </td>
-        <td <?php echo $this->getAttrs(array(
-            'width'   => '33%',
-            'rowspan' => '2'
-        )); ?>>
-
-            <?php if ($this->checkPosition('main')) :
-                echo $this->partial('main');
-            endif;
-
-            if ($this->checkPosition('payment')) :
-                echo $this->renderPosition('payment');
-            endif;
-
-            if ($this->checkPosition('shipping')) :
-                echo $this->renderPosition('shipping');
-            endif;
-
-            if ($this->checkPosition('shippingfield')) :
-                echo $this->renderPosition('shippingfield');
-            endif; ?>
-
+            <?php echo $this->renderPosition('items'); ?>
         </td>
     </tr>
 
     <tr>
         <td <?php echo $this->getAttrs(array(
-            'width'  => '65%',
-            'height' => '50%',
-            'valign' => 'middle'
+            'valign' => 'top'
         )); ?>>
             <?php if ($this->checkPosition('info')) :
                 echo $this->renderPosition('info');
@@ -81,14 +50,24 @@ defined('_JEXEC') or die('Restricted access');
         </td>
     </tr>
 
+    <tr>
+        <td <?php echo $this->getAttrs(array(
+            'valign' => 'top'
+        )); ?>>
+            <?php if ($this->checkPosition('main')) {
+                echo $this->partial('main');
+            } ?>
+
+            <?php echo $this->renderPosition('payment'); ?>
+            <?php echo $this->renderPosition('shipping'); ?>
+            <?php echo $this->renderPosition('shippingfield'); ?>
+        </td>
+    </tr>
+
 </table>
 
-<table>
-    <?php if ($this->checkPosition('advertise')) :
-        echo $this->renderPosition('advertise', array('style' => 'table'));
-    endif; ?>
-</table>
-
-
-
-
+<?php if ($this->checkPosition('advertise')) : ?>
+    <table>
+        <?php echo $this->renderPosition('advertise', array('style' => 'table')); ?>
+    </table>
+<?php endif; ?>
