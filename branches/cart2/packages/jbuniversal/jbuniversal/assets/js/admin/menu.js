774fe3582ebe5a9bc65b41c7a99717fdce52cccd
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-02-04 11:46:03 +0000

    Порядок инициализации скриптов админки был нарушен.

--- branches/cart2/packages/jbuniversal/jbuniversal/assets/js/admin/menu.js
+++ branches/cart2/packages/jbuniversal/jbuniversal/assets/js/admin/menu.js
@@ -11,68 +11,81 @@
 ;
 (function ($, window, document, undefined) {
 
-    $.fn.JBZooAdminMenu = function (options) {
+    /**
+     * JBZoo AdminMenu
+     */
+    JBZoo.widget('JBZoo.AdminMenu', {
+        'name' : '',
+        'items': {}
+    }, {
+        init: function ($this) {
 
-        var $obj = $(this),
-            html = '';
+            if (JBZoo.empty($this.options.items)) {
+                return false;
+            }
 
-        if ($obj.is('.disabled')) {
-            $obj.append('<li class="disabled"><a>' + options.name + '</a>');
-        } else {
+            var html = '';
 
-            $.each(options.items, function (parentKey, items) {
+            if ($this.el.is('.disabled')) {
+                $this.el.append('<li class="disabled"><a>' + $this.options.name + '</a>');
 
-                var attrs = '';
+            } else {
 
-                if (typeof items.target != "undefined") {
-                    attrs += 'target="' + items.target + '"';
-                }
+                $.each($this.options.items, function (parentKey, items) {
 
-                if (items == 'divider') {
-                    html += '<li class="separator divider"><span></span></li>';
+                    var attrs = '';
 
-                } else if (typeof items.children == 'undefined' || items.children.length == 0) {
-                    html += '<li><a ' + attrs + ' class="' + parentKey + '-item parent-link" href="' + items.url + '">' + items.name + '</a></li>';
-
-                } else {
-
-                    var classes = parentKey + '-item dropdown-toggle parent-link';
-                    if (typeof items.icon != "undefined") {
-                        attrs += ' style="background-image:url(' + items.icon + ');" ';
+                    if (typeof items.target != "undefined") {
+                        attrs += 'target="' + items.target + '"';
                     }
 
-                    html += '<li class="node dropdown-submenu">';
-                    html += '<a ' + attrs + ' class="' + classes + '" data-toggle="dropdown" href="' + items.url + '">' +
-                    items.name + '</a><ul class="dropdown-menu">';
+                    if (items == 'divider') {
+                        html += '<li class="separator divider"><span></span></li>';
 
-                    $.each(items.children, function (childKey, item) {
+                    } else if (typeof items.children == 'undefined' || items.children.length == 0) {
+                        html += '<li><a ' + attrs + ' class="' + parentKey + '-item parent-link" href="' + items.url + '">' + items.name + '</a></li>';
 
-                        var innerAttrs = '';
-                        if (typeof item.target != "undefined") {
-                            innerAttrs += ' target="' + items.target + '" ';
+                    } else {
+
+                        var classes = parentKey + '-item dropdown-toggle parent-link';
+                        if (typeof items.icon != "undefined") {
+                            attrs += ' style="background-image:url(' + items.icon + ');" ';
                         }
 
-                        if (item == 'divider') {
-                            html += '<li class="separator divider"><span></span></li>';
+                        html += '<li class="node dropdown-submenu">';
+                        html += '<a ' + attrs + ' class="' + classes + '" data-toggle="dropdown" href="' + items.url + '">' +
+                        items.name + '</a><ul class="dropdown-menu">';
 
-                        } else {
-                            html += '<li><a ' + innerAttrs + ' class="' + childKey + '-item" href="' + item.url + '">' + item.name + '</a></li>';
-                        }
-                    });
+                        $.each(items.children, function (childKey, item) {
 
-                    html += '</ul></li>';
-                }
+                            var innerAttrs = '';
+                            if (typeof item.target != "undefined") {
+                                innerAttrs += ' target="' + items.target + '" ';
+                            }
 
-            });
+                            if (item == 'divider') {
+                                html += '<li class="separator divider"><span></span></li>';
 
-            $obj.append(
-                '<li class="dropdown" id="jbzoo-adminmenu">' +
-                '<a class="dropdown-toggle" data-toggle="dropdown" href="#">' +
-                options.name +
-                ' <span class="caret"></span></a>' +
-                '<ul class="dropdown-menu">' + html + '</ul></li>');
+                            } else {
+                                html += '<li><a ' + innerAttrs + ' class="' + childKey + '-item" href="' + item.url + '">' + item.name + '</a></li>';
+                            }
+                        });
+
+                        html += '</ul></li>';
+                    }
+
+                });
+
+                $this.el.append(
+                    '<li class="dropdown" id="jbzoo-adminmenu">' +
+                    '<a class="dropdown-toggle" data-toggle="dropdown" href="#">' +
+                    $this.options.name +
+                    ' <span class="caret"></span></a>' +
+                    '<ul class="dropdown-menu">' + html + '</ul></li>'
+                );
+            }
         }
 
-    };
+    });
 
 })(jQuery, window, document);
