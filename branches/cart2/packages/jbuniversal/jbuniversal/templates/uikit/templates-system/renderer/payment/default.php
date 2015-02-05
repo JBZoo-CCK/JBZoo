234b569fce590438c6c3cbea834d7bda0872566d
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-02-05 14:07:36 +0000

    #ADD: Адаптивные колонки платежных систем в корзине UIKIT

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/templates-system/renderer/payment/default.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+
+if ($this->checkPosition('list')) : ?>
+    <div class="jbcart-payment clearfix">
+        <p class="jbcart-title"><?php echo JText::_('JBZOO_CART_PAYPMENT_TITLE'); ?></p>
+        <?php
+        echo $this->renderPosition('list', array(
+            'style' => 'order.payment',
+            'rowAttrs' => array(
+                'class' => 'uk-grid',
+                'data-uk-grid-margin' => true
+            ),
+            'column' => 3
+        ));
+        ?>
+    </div>
+<?php endif;
