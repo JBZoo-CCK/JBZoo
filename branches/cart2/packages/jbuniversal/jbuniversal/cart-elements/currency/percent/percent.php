2228896c83fa187bf5c998ebcc39357a31cc6fc5
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-02-10 14:41:24 +0000

    Элемент для процентов Фиксы в форматировании процентов

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/currency/percent/percent.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Denis Smetannikov <denis@jbzoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Class JBCartElementCurrencyPercent
+ */
+class JBCartElementCurrencyPercent extends JBCartElementCurrency
+{
+    /**
+     * @param null $currency
+     * @return array|void
+     */
+    public function _loadData($currency = null)
+    {
+        return array();
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return JBCartValue::PERCENT;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrencyName()
+    {
+        return JText::_('JBZOO_CART_CURRENCY_PERCENT');
+    }
+
+    public function checkCurrency($currency)
+    {
+        return $currency == $this->getCode();
+    }
+
+}
