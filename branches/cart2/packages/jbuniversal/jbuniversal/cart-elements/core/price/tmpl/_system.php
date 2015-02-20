283c6b13690237aaef536019a8f2afdb773f8a10
tapakan <tapakan@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-02-20 23:43:08 +0000

    * Удалены простые шаблоны у элементов цены и добавлены в родитель * Фикс элемента jbcolor - Добавлен loadAssets

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/core/price/tmpl/_system.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$type   = $this->getElementType();
+$isCore = ($this->isCore() ? 'core' : 'simple');
+
+$classes = array(
+    'jbprice-param-' . $type . ' jbprice-' . $isCore . '-param jbprice-param',
+    'jsElement jsPriceElement js' . ucfirst($type) . ' js' . ucfirst($isCore)
+);
+
+$attr = array(
+    'data-identifier' => $this->identifier
+);
+
+echo
+    '<div class="' . implode(' ', $classes) . '"' . $this->_jbhtml->buildAttrs($attr) . '>'
+    . $html .
+    '</div>';
