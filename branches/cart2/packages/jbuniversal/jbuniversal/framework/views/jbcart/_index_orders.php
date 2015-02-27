2e3f5fa86bab718a46170f0e6b143adec2a41af2
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-02-27 14:28:50 +0000

    Список заказов в админке

--- branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jbcart/_index_orders.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jbcart/_index_orders.php
@@ -1,2 +1,40 @@
-<h2>Последние заказы</h2>
-Soon
+<?php
+if (!empty($this->orders)) {
+
+    $html = array(
+        '<br><h2>5 последних заказов</h2>',
+        '<table class="uk-table"><thead><tr>',
+        '<th>' . JText::_('JBZOO_ADMIN_NAME') . '</th>',
+        '<th>' . JText::_('JBZOO_ADMIN_CREATED') . '</th>',
+        '<th>' . JText::_('JBZOO_ADMIN_STATUS') . '</th>',
+        '<th>' . JText::_('JBZOO_ADMIN_TOTAL') . '</th>',
+        '<th>' . JText::_('JBZOO_ADMIN_ORDER_COMMENT') . '</th>',
+        '</tr></thead>',
+    );
+
+    foreach ($this->orders as $order) {
+        $html[] = '<tr>';
+
+        $html[] = '<td>';
+        $html[] = '<a href="' . $order->getUrl() . '">№' . $order->getName() . '</a> ' . JText::_('JBZOO_BY');
+        if ($user = $order->getAuthor()) {
+            $href   = $this->app->component->users->link(array('task' => 'user.edit', 'layout' => 'edit', 'view' => 'user', 'id' => $user->id));
+            $html[] = '<i><a href="' . $href . '">' . $user->name . '</a></i>';
+        } else {
+            $html[] = '<i>' . JText::_('JBZOO_ANONYM') . '</i>';
+        }
+        $html[] = '</td>';
+
+        $html[] = '<td>' . $this->app->html->_('date', $order->created, JText::_('DATE_FORMAT_LC2'), $this->app->date->getOffset()) . '</td>';
+        $html[] = '<td>' . $order->getStatus()->getName() . '</td>';
+        $html[] = '<td>' . $order->getTotalSum(true) . '</td>';
+        $html[] = '<td>' . (($order->comment) ? $order->comment : ' - ') . '</td>';
+
+        $html[] = '</tr>';
+    }
+
+    $html[] = '</table>';
+    echo implode("\n", $html);
+}
+
+
