a185b3a23604c232bf996e33951275cf7fb21695
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-03-02 16:21:42 +0000

    Удалены древние функции

--- branches/cart2/packages/jbuniversal/jbuniversal/framework/helpers/jbcache.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/helpers/jbcache.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
- *
  * @package     jbzoo
  * @version     2.x Pro
  * @author      JBZoo App http://jbzoo.com
@@ -25,60 +24,6 @@ class JBCacheHelper extends AppHelper
     protected $_cache = null;
 
     /**
-     * Start cache process
-     * @deprecated
-     * @param mixed $params
-     * @param string $type
-     * @return mixed
-     */
-    public function start($params = null, $type = null)
-    {
-        return null; // disibled
-        !$type && $type = $this->app->jbrequest->get('view');
-        !$type && $type = $this->app->jbrequest->get('task');
-
-        $application = $this->app->zoo->getApplication();
-        if ($application) {
-            $group = 'jbzoo_' . $application->alias . '_' . $type;
-        } else {
-            $group = 'jbzoo_' . $type;
-        }
-
-        $this->_cache = JFactory::getCache($group, 'output');
-
-        $result = $this->_cache->start($this->_getKey($params));
-
-        return $result;
-    }
-
-    /**
-     * Stop cache
-     * @deprecated
-     */
-    public function stop()
-    {
-        return null; // disibled
-        return $this->_cache->end();
-    }
-
-    /**
-     * Create uniq cache key
-     * @param array $params
-     * @return string
-     */
-    public function _getKey($params = null)
-    {
-        $result   = array();
-        $result[] = $this->app->jbwrapper->attrs();
-        $result[] = serialize($params);
-        $result[] = serialize($_GET);
-        $result[] = $this->app->jbrequest->get('tmpl', 'index');
-        $result[] = $this->app->jbrequest->get('page', 1);
-
-        return implode('||', $result);
-    }
-
-    /**
      * Check config, is enabled joomla caching
      * @return int
      */
@@ -91,10 +36,10 @@ class JBCacheHelper extends AppHelper
     /**
      * Set data to cache storage by key
      * @param string $key
-     * @param mixed $data
+     * @param mixed  $data
      * @param string $group
-     * @param bool $isForce
-     * @param array $params
+     * @param bool   $isForce
+     * @param array  $params
      * @return bool
      */
     public function set($key, $data, $group = 'default', $isForce = false, array $params = array())
@@ -117,8 +62,8 @@ class JBCacheHelper extends AppHelper
      * Get cache data by key
      * @param string $key
      * @param string $group
-     * @param bool $isForce
-     * @param array $params
+     * @param bool   $isForce
+     * @param array  $params
      * @return null
      */
     public function get($key, $group = 'default', $isForce = false, array $params = array())
@@ -156,7 +101,22 @@ class JBCacheHelper extends AppHelper
      */
     protected function _simpleHash($var)
     {
-        return sha1(serialize($var));
+        return serialize($var);
     }
 
+    /**
+     * @deprecated
+     */
+    public function start()
+    {
+        return null; // disibled
+    }
+
+    /**
+     * @deprecated
+     */
+    public function stop()
+    {
+        return null; // disibled
+    }
 }
