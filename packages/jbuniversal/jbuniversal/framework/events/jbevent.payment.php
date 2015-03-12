bdb4646bbf6b01ff072cd1703f68de3978c84955
Denis Smetannikov <denis@jbzoo.com>
2015-03-12 06:37:44 +0000

    * удалены старые cобытия после оплаты

--- packages/jbuniversal/jbuniversal/framework/events/jbevent.payment.php
+++ packages/jbuniversal/jbuniversal/framework/events/jbevent.payment.php
@@ -26,41 +26,6 @@ class JBEventPayment extends JBEvent
      */
     public static function callback($event)
     {
-        $app    = self::app();
-        $params = $event->getParameters();
-        $app->jbeventmanager->fireListeners();
-
-        $appParams = $params['appParams'];
-        $item      = $params['item'];
-        $subject   = JText::_('JBZOO_CART_ORDER_PAYMENT_COMPLITE');
-
-        if ((int)$appParams->get('notificaction-payment', 1)) {
-
-            // to admin
-            $adminEmail = $params['appParams']->get('global.jbzoo_cart_config.admin-email');
-            if ($adminEmail) {
-                $adminLayout = $appParams->get('global.jbzoo_cart_config.email-admin-layout-payment');
-                $app->jbemail->sendByItem($adminEmail, $subject, $item, $adminLayout);
-            }
-
-            // to user email from profile
-            $userEmail = JFactory::getUser($item->created_by)->email;
-            if ($userEmail) {
-                $userLayout = $appParams->get('global.jbzoo_cart_config.email-user-layout-payment');
-                $app->jbemail->sendByItem($userEmail, $subject, $item, $userLayout);
-            }
-
-            // to email from order field
-            $emailElement = $appParams->get('global.jbzoo_cart_config.element-useremail');
-            if ($element = $item->getElement($emailElement)) {
-                $data = $element->data();
-                if (isset($data[0]['value']) && !empty($data[0]['value'])) {
-                    $userLayout = $appParams->get('global.jbzoo_cart_config.email-user-layout-payment');
-                    $app->jbemail->sendByItem($data[0]['value'], $subject, $item, $userLayout);
-                }
-            }
-        }
-
     }
 
 }
