dd1296a2d868d3fa2e35b8690502c449b3311449
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-03-20 14:30:06 +0000

    Сортировка заказов в панели управления

--- branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jborder/index.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/views/jborder/index.php
@@ -12,40 +12,41 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+
+$order    = $this->filter->get('filter_order', 'id');
+$orderDir = $this->filter->get('filter_order_Dir', 'desc');
 ?>
-    <form class="items-default" action="<?php echo $this->app->jbrouter->admin(); ?>" method="get" name="adminForm"
-          id="adminForm" accept-charset="utf-8">
 
-        <?php
+<form class="items-default" action="<?php echo $this->app->jbrouter->admin(); ?>" method="get" name="adminForm"
+      id="adminForm" accept-charset="utf-8">
 
-        echo $this->partial('index_filter', array(
-            'filter' => $this->filter,
+    <?php echo $this->partial('index_filter', array(
+        'filter' => $this->filter,
+    )); ?>
+
+
+    <?php if ($this->pagination->total > 0) {
+        echo $this->partial('index_orderlist', array(
+            'orderList' => $this->orderList,
+            'filter'    => $this->filter,
         ));
+    } else {
+        echo $this->partial('message', array(
+            'title'   => JText::_('JBZOO_ADMIN_ORDER_NO_ITEMS_YET'),
+            'message' => '',
+        ));
+    } ?>
 
-        if ($this->pagination->total > 0) {
-            echo $this->partial('index_orderlist', array(
-                'orderList' => $this->orderList,
-                'filter'    => $this->filter,
-            ));
 
-        } else {
-            echo $this->partial('message', array(
-                'title'   => JText::_('JBZOO_ADMIN_ORDER_NO_ITEMS_YET'),
-                'message' => '',
-            ));
-        }
+    <input type="hidden" name="option" value="<?php echo $this->app->jbrequest->get('option'); ?>" />
+    <input type="hidden" name="controller" value="<?php echo $this->app->jbrequest->getCtrl(); ?>" />
+    <input type="hidden" name="task" value="" />
+    <input type="hidden" name="boxchecked" value="0" />
+    <input type="hidden" name="filter_order" value="<?php echo $order; ?>" />
+    <input type="hidden" name="filter_order_Dir" value="<?php echo $orderDir; ?>" />
 
-        ?>
+    <?php echo $this->app->html->_('form.token'); ?>
 
-        <input type="hidden" name="option" value="<?php echo $this->app->jbrequest->get('option'); ?>" />
-        <input type="hidden" name="controller" value="<?php echo $this->app->jbrequest->getCtrl(); ?>" />
-        <input type="hidden" name="task" value="" />
-        <input type="hidden" name="boxchecked" value="0" />
-        <input type="hidden" name="filter_order" value="<?php echo $this->filter->get('filter_order', 'id'); ?>" />
-        <input type="hidden" name="filter_order_Dir"
-               value="<?php echo $this->filter->get('filter_order_Dir', 'desc'); ?>" />
-        <?php echo $this->app->html->_('form.token'); ?>
-
-    </form>
+</form>
 
 <?php echo $this->partial('footer'); ?>
