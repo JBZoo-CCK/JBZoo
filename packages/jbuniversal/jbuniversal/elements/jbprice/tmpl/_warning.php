8069b81b5f4118c9d9f8e7bd8e364828e28e7564
Alex <t_tapak@yahoo.com>
2015-03-26 20:21:13 +0000

    * checkbox in filter * code formating

--- packages/jbuniversal/jbuniversal/elements/jbprice/tmpl/_warning.php
+++ packages/jbuniversal/jbuniversal/elements/jbprice/tmpl/_warning.php
@@ -12,9 +12,5 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-$link = '<a target="_blank" href="'
-    . $link . '">'
-    . $message . '</a>';
-echo '<em>'
-    . JText::sprintf('JBZOO_PRICE_EDIT_ERROR_NO_ELEMENTS', $this->app->jbenv->isSite() ? '' : $link)
-    . '</em>';
+$link = '<a target="_blank" href="' . $link . '">' . $message . '</a>';
+echo '<em>' . JText::sprintf('JBZOO_PRICE_EDIT_ERROR_NO_ELEMENTS', $this->app->jbenv->isSite() ? '' : $link) . '</em>';
