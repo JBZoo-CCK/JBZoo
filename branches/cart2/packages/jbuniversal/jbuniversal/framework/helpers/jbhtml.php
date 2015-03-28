dc19d1ea1e47f8335c220ace225b2e960b17e97b
tapakan <tapakan@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-03-28 20:59:49 +0000

    * класс jsNoCurrencyToggle добавлен к шаблону слайдера

--- branches/cart2/packages/jbuniversal/jbuniversal/framework/helpers/jbhtml.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/helpers/jbhtml.php
@@ -519,27 +519,27 @@ class JBHtmlHelper extends AppHelper
 
         $jbVal_0 = JBCart::val($value[0]);
         $jbVal_1 = JBCart::val($value[1]);
-        $val_0 = $jbVal_0->val($currency);
-        $val_1 = $jbVal_1->val($currency);
+        $val_0 = $jbVal_0->val();
+        $val_1 = $jbVal_1->val();
         $html   = array();
-        $html[] = '<div id="' . $idTag . '" class="jsSlider jbzoo-slider">';
+        $html[] = '<div id="' . $idTag . '" class="jsSlider jbzoo-slider jsNoCurrencyToggle">';
         $html[] = '<div class="jsSliderWrapper jbzoo-slider-wrapper"></div>';
 
         $html[] = '<div class="jsSliderBox jbslider-box">';
-        $html[] = '<span class="jsSliderLabel jsSliderLabel-0 jbslider-label">' . $jbVal_0->html($currency) . '</span>'
-            . $this->text(null, $val_0, array(
+        $html[] = '<span class="jsSliderLabel jsSliderLabel-0 jbslider-label">' . $jbVal_0->html() . '</span>'
+            . $this->text(null, $jbVal_0->val(), array(
                 'class' => 'jsSlider-0 jsSliderMin jsSliderInput jbslider-min jbslider-input',
             ));
         $html[] = '</div>';
 
         $html[] = '<div class="jsSliderBox jbslider-box">';
-        $html[] = '<span class="jsSliderLabel jsSliderLabel-1 jbslider-label"/>' . $jbVal_1->html($currency) . '</span>'
-            . $this->text(null, $val_1, array(
+        $html[] = '<span class="jsSliderLabel jsSliderLabel-1 jbslider-label"/>' . $jbVal_1->html() . '</span>'
+            . $this->text(null, $jbVal_1->val(), array(
                 'class' => 'jsSlider-1 jsSliderMax jsSliderInput jbslider-max jbslider-input',
             ));
         $html[] = '</div>';
 
-        $html[] = $this->hidden($name, $val_0 . '/' . $val_1, array(
+        $html[] = $this->hidden($name, $jbVal_0->val() . '/' . $jbVal_1->val(), array(
             'class' => 'jsSliderValue'
         ));
 
