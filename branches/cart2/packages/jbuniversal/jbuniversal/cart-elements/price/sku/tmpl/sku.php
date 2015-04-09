e58ee4895b321d14e8d44d261e066655d0452cb3
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-04-09 09:40:54 +0000

    Мелочи в элементе артикула

--- branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/price/sku/tmpl/sku.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/price/sku/tmpl/sku.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
- *
  * @package     jbzoo
  * @version     2.x Pro
  * @author      JBZoo App http://jbzoo.com
@@ -13,8 +12,6 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-if ($sku) : ?>
-    <div class="jbprice-sku">
-        <span class="sku"><?php echo $sku; ?></span>
-    </div>
-<?php endif;
+?>
+
+<div class="jbprice-sku"><?php echo $this->getValue(true); ?></div>
