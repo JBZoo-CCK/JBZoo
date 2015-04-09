1d58ab66611c3e70e8fc1997e52d4e5ea1e8f32e
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-04-09 06:07:27 +0000

    классы в шаблонах цены

--- branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/price/value/tmpl/full-div.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/price/value/tmpl/full-div.php
@@ -16,8 +16,8 @@ defined('_JEXEC') or die('Restricted access');
 
 <?php if ($discount->isEmpty()) : ?>
 
-    <div class="jbprice-value jbprice-value-div no-border">
-        <div>
+    <div class="jbprice-value jbprice-value-div">
+        <div class="jbprice-value-row">
             <span class="jbprice-value-label"><?php echo JText::_('JBZOO_ELEMENT_PRICE_VALUE_LABEL_TOTAL'); ?></span>
             <span class="jbprice-value-total"><?php echo $total->html($currency); ?></span>
         </div>
@@ -26,15 +26,15 @@ defined('_JEXEC') or die('Restricted access');
 <?php elseif ($discount->isPositive()) : ?>
 
     <div class="jbprice-value jbprice-value-div no-border">
-        <div>
+        <div class="jbprice-value-row">
             <span class="jbprice-value-label"><?php echo JText::_('JBZOO_ELEMENT_PRICE_VALUE_LABEL_PRICE'); ?></span>
             <span class="jbprice-value-price"><?php echo $price->html($currency); ?></span>
         </div>
-        <div>
+        <div class="jbprice-value-row">
             <span class="jbprice-value-label"><?php echo JText::_('JBZOO_ELEMENT_PRICE_VALUE_LABEL_TOTAL'); ?></span>
             <span class="jbprice-value-total"><?php echo $total->html($currency); ?></span>
         </div>
-        <div>
+        <div class="jbprice-value-row">
             <span class="jbprice-value-label"><?php echo JText::_('JBZOO_ELEMENT_PRICE_VALUE_LABEL_SAVE'); ?></span>
             <span class="jbprice-value-save">
                 <span class="jbprice-value-save-value"><?php echo $save->html($currency); ?></span>
