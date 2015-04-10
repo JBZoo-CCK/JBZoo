77a61946aec3c87169bbf13e074c859edf07917d
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-04-10 13:23:44 +0000

    Пометки о классах  цены на будущее

--- branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/core/price/tmpl/_system.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/core/price/tmpl/_system.php
@@ -18,9 +18,10 @@ $isCore = ($this->isCore() ? 'core' : 'simple');
 $attr = array(
     'data-identifier' => $this->identifier,
     'class'           => array(
-        'jbprice-param',
-        'jbprice-' . $type, // very IMPORTANT class for element templates
-        'jbprice-' . $isCore . '-param',
+        'jbprice-' . $type, // very IMPORTANT class for element templates (DON'T REMOVE!)
+        'jbprice-param',                    // TODO PLZ, kill me (but JS bugs!)
+        'jbprice-param-' . $type,           // TODO PLZ, kill me (but JS bugs!)
+        'jbprice-' . $isCore . '-param',    // TODO PLZ, kill me (but JS bugs!)
         'jsElement',
         'jsPriceElement',
         'js' . ucfirst($type),
