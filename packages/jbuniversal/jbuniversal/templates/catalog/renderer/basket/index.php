e4234490d97a7f8769ab2cd26e7eb0692ed19fb7
Denis Smetannikov <denis@jbzoo.com>
2015-04-17 10:09:37 +0000

    Фиксы шаблона формы заказа, не отображались поля заказа

--- packages/jbuniversal/jbuniversal/templates/catalog/renderer/basket/index.php
+++ packages/jbuniversal/jbuniversal/templates/catalog/renderer/basket/index.php
@@ -14,53 +14,60 @@ defined('_JEXEC') or die('Restricted access');
 
 $view = $this->getView();
 
-if ((int)$view->application->params->get('global.config.column_heightfix', 0)) {
-    $this->app->jbassets->heightFix();
-}
-
-$this->app->jbassets->widget('.jbzoo .jsJBZooCart', 'JBZoo.Cart', array(
-    'text_remove_all'  => JText::_('JBZOO_CART_CLEANUP'),
-    'text_remove_item' => JText::_('JBZOO_CART_REMOVE_ITEM'),
-    'url_shipping'     => $this->app->jbrouter->basketShipping(),
-    'url_quantity'     => $this->app->jbrouter->basketQuantity(),
-    'url_delete'       => $this->app->jbrouter->basketDelete(),
-    'url_clear'        => $this->app->jbrouter->basketClear(),
-    'items'            => $view->order->getItems(false),
-));
 
 ?>
 
-
 <?php if (count($view->items) == 0) : ?>
-    <p><?php echo JText::_('JBZOO_CART_ITEMS_NOT_FOUND'); ?></p>
-<?php else: ?>
+    <p class="jbcart-empty-message"><?php echo JText::_('JBZOO_CART_ITEMS_NOT_FOUND'); ?></p>
 
-    <?php
-    $isFormEmpty = empty($view->payment)
-        && empty($view->shipping)
-        && empty($view->shippingFields)
-        && !$view->formRenderer->checkPosition(JBCart::DEFAULT_POSITION);
+<?php else:
+
+    if ((int)$view->application->params->get('global.config.column_heightfix', 0)) {
+        $this->app->jbassets->heightFix();
+    }
+
+    $this->app->jbassets->widget('.jbzoo .jsJBZooCart', 'JBZoo.Cart', array(
+        'text_remove_all'  => JText::_('JBZOO_CART_CLEANUP'),
+        'text_remove_item' => JText::_('JBZOO_CART_REMOVE_ITEM'),
+        'url_shipping'     => $this->app->jbrouter->basketShipping(),
+        'url_quantity'     => $this->app->jbrouter->basketQuantity(),
+        'url_delete'       => $this->app->jbrouter->basketDelete(),
+        'url_clear'        => $this->app->jbrouter->basketClear(),
+        'items'            => $view->order->getItems(false),
+    ));
+
+    $formAttrs = array(
+        'action'         => $this->app->jbrouter->cartOrderCreate(),
+        'method'         => 'post',
+        'name'           => 'jbcartForm',
+        'accept-charset' => 'utf-8',
+        'enctype'        => 'multipart/form-data',
+        'class'          => array(
+            'jbcart',
+            'jsJBZooCart',
+        ),
+    );
+
     ?>
-
-    <form action="<?php echo $this->app->jbrouter->cartOrderCreate(); ?>" class="jbcart jsJBZooCart" method="post"
-          name="jbcartForm" accept-charset="utf-8" enctype="multipart/form-data">
+    <form <?php echo $this->app->jbhtml->buildAttrs($formAttrs); ?>>
 
         <?php echo $this->partial('basket', 'validators'); ?>
         <?php echo $this->partial('basket', 'table'); ?>
-
-        <?php if (!$isFormEmpty) : ?>
-            <?php echo $this->partial('basket', 'form'); ?>
-            <?php echo $this->partial('basket', 'shipping'); ?>
-            <?php echo $this->partial('basket', 'payment'); ?>
-        <?php endif; ?>
-
+        <?php echo $this->partial('basket', 'form'); ?>
+        <?php echo $this->partial('basket', 'shipping'); ?>
+        <?php echo $this->partial('basket', 'payment'); ?>
         <?php echo $this->partial('basket', 'buttons'); ?>
 
-        <input type="hidden" name="option" value="com_zoo" />
-        <input type="hidden" name="controller" value="basket" />
-        <input type="hidden" name="task" value="index" />
-        <input type="hidden" name="Itemid" value="<?php echo $view->Itemid; ?>" />
-        <?php echo $this->app->html->_('form.token'); ?>
+        <?php
+        // system fields
+        echo $this->app->jbhtml->hiddens(array(
+                'option'     => 'com_zoo',
+                'controller' => 'basket',
+                'task'       => 'index',
+                'Itemid'     => $view->Itemid,
+            ))
+            . $this->app->html->_('form.token');
+        ?>
     </form>
 
 <?php endif;
