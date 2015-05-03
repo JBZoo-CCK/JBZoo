d0b56a257214776a1a5dcd95ef9b8ca3cd85b62c
Denis Smetannikov <denis@jbzoo.com>
2015-05-03 08:16:44 +0000

    Скрытый шаблон для переключателя валюты

---
+++ packages/mod_jbzoo_currency/tmpl/hidden.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Denis Smetannikov <denis@jbzoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$currency = $params->get('currency_default', 'eur');
+$target   = $params->get('switcher_target', '.jbzoo');
+?>
+
+<script>
+    jQuery(function ($) {
+        $('.jsMoney', $('<?php echo $target;?>'))
+            .filter(function () {
+                return $(this).closest('.jsNoCurrencyToggle').length == 0;
+            })
+            .JBZooMoney({}).JBZooMoney('convert', '<?php echo $currency;?>');
+    });
+</script>
