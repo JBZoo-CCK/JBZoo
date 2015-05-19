5f3bcde0014a79e8aefe85c6cf3648aed4c84151
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-05-19 11:26:53 +0000

    новый тип элемента - хук

---
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/hook/jbdumplog/jbdumplog.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Denis Smetannikov <denis@jbzoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+
+/**
+ * Class JBCartElementHookJBDumpLog
+ */
+class JBCartElementHookJBDumpLog extends JBCartElementHook
+{
+
+    /**
+     * @param $params
+     */
+    public function notify($params = array())
+    {
+        if (class_exists('jbdump')) {
+
+            $order     = $this->getOrder();
+            $eventName = $this->getEvent()->getName();
+            $message   = $eventName . '  / ' . $this->getName();
+
+            if ($order && $order->id) {
+                $message .= ' (id:' . $order->id . ')';
+
+            } else if (!empty($order)) {
+                $message .= ' (id:0)';
+
+            } else if (!empty($order)) {
+                $message .= ' (null)';
+            }
+
+            jbdump::log($message);
+        }
+    }
+
+}
