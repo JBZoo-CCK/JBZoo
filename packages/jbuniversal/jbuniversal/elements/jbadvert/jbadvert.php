68ba92d728cb16266376f09c28384b1a11e8b026
Denis Smetannikov <denis@jbzoo.com>
2015-05-21 14:20:53 +0000

    Полное обновление JBAdvert

--- packages/jbuniversal/jbuniversal/elements/jbadvert/jbadvert.php
+++ packages/jbuniversal/jbuniversal/elements/jbadvert/jbadvert.php
@@ -6,7 +6,7 @@
  * @author      JBZoo App http://jbzoo.com
  * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
  * @license     http://jbzoo.com/license-pro.php JBZoo Licence
- * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ * @coder       Denis Smetannikov <denis@jbzoo.com>
  */
 
 // no direct access
@@ -17,42 +17,24 @@ defined('_JEXEC') or die('Restricted access');
  */
 class ElementJBAdvert extends Element implements iSubmittable
 {
+
+    const MODE_PUBLISH    = 'publish';
+    const MODE_PRIORITY   = 'priority';
+    const MODE_CATEGORY   = 'category';
+    const MODE_EXPIREDATE = 'expiredate';
+    const MODE_ELEMENT    = 'element';
+    const MODE_PHP        = 'php';
+
+    const MODIFIED_NO   = 0;
+    const MODIFIED_YES  = 1;
+    const MODIFIED_EXEC = 2;
+
     /**
-     * Array of element values
-     * @type array
+     * @type string
      */
-    public $data = array();
+    protected $_sesKey = 'data';
 
     /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->data = array(
-            '0' => JText::_('JBZOO_JBADVERT_VALUE_ACTIVATE')
-        );
-    }
-
-    /**
-     * Modify item
-     */
-    public function modify()
-    {
-        $data = $this->data();
-        if (!empty($data)) {
-            $item = $this->getItem();
-            foreach ($data as $key => $value) {
-                if (!$item->state && array_key_exists($value, $this->data)) {
-                    $item->setState(1, true);
-                }
-            }
-        }
-    }
-
-    /**
-     * Check if elements value is set
      * @param array $params
      * @return bool
      */
@@ -67,23 +49,14 @@ class ElementJBAdvert extends Element implements iSubmittable
      */
     public function edit($params = array())
     {
+        JFactory::getSession()->set($this->_getSaveKey(), $this->data(), __CLASS__);
+
         if ($layout = $this->getLayout('edit.php')) {
             return self::renderLayout($layout, array(
                 'params' => $params,
-                'data'   => $this->addPrices($this->data)
             ));
         }
 
-        return false;
-    }
-
-    /**
-     * Renders the element
-     * @param array $params Render parameters
-     * @return string|void
-     */
-    public function render($params = array())
-    {
         return false;
     }
 
@@ -94,20 +67,14 @@ class ElementJBAdvert extends Element implements iSubmittable
      */
     public function renderSubmission($params = array())
     {
-        $tpl  = 'submission.php';
-        $data = $this->data;
-
-        if ($this->getItem()->state) {
-            unset($data['0']);
+        $elementLayout = 'submission-not-modified.php';
+        if ($this->_isModified()) {
+            $elementLayout = 'submission-modified.php';
         }
 
-        if (empty($data)) {
-            $tpl = 'no_services.php';
-        }
-        if ($layout = $this->getLayout($tpl)) {
+        if ($layout = $this->getLayout($elementLayout)) {
             return self::renderLayout($layout, array(
                 'params' => $params,
-                'data'   => $this->addPrices($data)
             ));
         }
 
@@ -123,20 +90,230 @@ class ElementJBAdvert extends Element implements iSubmittable
      */
     public function validateSubmission($values, $params)
     {
-        $options  = array('required' => $params->get('required'));
-        $messages = array('required' => 'Please choose an option.');
 
-        $values = $this->app->validator
-            ->create('foreach', $this->app->validator->create('string', $options, $messages), $options, $messages)
-            ->clean($values->get('value'));
+        $mode    = $this->_getMode();
+        $price   = $this->_getPrice()->data(true);
+        $mParams = $this->_getModifiersParams($mode);
 
-        foreach ($values as $key => $value) {
-            if (!array_key_exists($value, $this->data)) {
-                unset($values[$key]);
+        $values = array(
+            'mode'        => $mode,
+            'price'       => $price,
+            'params'      => $mParams,
+            'order_id'    => 0,
+            'modified'    => $this->_getLastModified(),
+            'is_modified' => 0,
+        );
+
+        return $values;
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getLastModified()
+    {
+        $date = $this->get('modified', JText::_('JBZOO_JBADVERT_MODIFIED_DATE_UNDEFIED'));
+        return $this->app->jbdate->toHuman($date);
+    }
+
+    /**
+     * @param array $data
+     */
+    public function bindData($data = array())
+    {
+        $saveData = $data;
+        if ($this->getItem()) {
+            $newData = JFactory::getSession()->get($this->_getSaveKey(), null, __CLASS__);
+        }
+
+        if (!empty($newData)) {
+            $saveData = $newData;
+        }
+
+        $saveData = array_merge($saveData, $data);
+        if ($saveData['is_modified'] == self::MODIFIED_EXEC) {
+            parent::bindData($saveData);
+            $this->modifyItem();
+        } else {
+            parent::bindData($saveData);
+        }
+
+    }
+
+    /**
+     * Modify item
+     * @param JSONData|array   $params
+     * @param JBCartOrder|null $order
+     * @return bool
+     */
+    public function modifyItem($params = null, $order = null)
+    {
+        if ($this->_isModified()) {
+            return false;
+        }
+
+        $item = $this->getItem();
+        if (empty($item)) {
+            return false;
+        }
+
+        $mode = $this->_getMode();
+        /** @type JSONData $mParams */
+        $mParams = $this->app->data->create($this->get('params', array()));
+        /** @type JBDateHelper $jbdate */
+        $jbdate = $this->app->jbdate;
+
+        if ($mode == self::MODE_PUBLISH) { // publish
+            $item->setState(1, false);
+
+
+        } else if ($mode == self::MODE_CATEGORY) { // change category and application
+
+            $categoryId = (int)$mParams->find('item_category_value.category', 0);
+            $appId      = (int)$mParams->find('item_category_value.app', 0);
+
+            // set new app
+            $item->application_id = $appId;
+
+            // and new category
+            if (0) {
+                $this->app->category->saveCategoryItemRelations($item, $categoryId); // need check ACL
+            } else {
+                $item->getParams()->set('config.primary_category', $categoryId);
+                $this->app->table->item->save($item);
+                $sql = "DELETE FROM " . ZOO_TABLE_CATEGORY_ITEM . " WHERE item_id=" . (int)$item->id;
+                $this->app->database->query($sql);
+
+                $sql = "INSERT INTO " . ZOO_TABLE_CATEGORY_ITEM . " (category_id, item_id) VALUES (" . $categoryId . ',' . (int)$item->id . ')';
+                $this->app->database->query($sql);
+            }
+
+            // TODO admin page save bug
+
+        } else if ($mode == self::MODE_ELEMENT) {  // set new data to element
+            /** @type Element|ElementRepeatable $element */
+            $element = $item->getElement($mParams->get('item_element_id'));
+            if ($element) {
+
+                $value = JString::trim($mParams->get('item_element_value'));
+
+                if (strpos($value, '{') === 0 && strpos($value, '}') > 1) {
+                    $value = json_decode($value, true);
+                } else {
+                    if ($element instanceof ElementOption) {
+                        $value = array('option' => array($this->app->string->sluggify($value)));
+                    } else if ($element instanceof ElementRepeatable) {
+                        $value = array(array('value' => $value));
+                    } else {
+                        $value = array('value' => $value);
+                    }
+                }
+
+                $element->bindData($value);
+            }
+
+
+        } else if ($mode == self::MODE_PHP) { // execute php code
+            eval($mParams->get('item_php_eval'));
+
+
+        } else if ($mode == self::MODE_PRIORITY) { // change priority
+            $item->priority = (int)$mParams->get('item_priority_value', -1);
+
+
+        } else if ($mode == self::MODE_EXPIREDATE) { // set new expire date (publish date)
+
+            $addTime = 86400 * $this->app->jbvars->number($mParams->get('item_expiredate_timeout', 30));
+            $oldDate = time();
+
+            if ($time = strtotime($item->publish_down)) {
+                $oldDate = $time;
+            }
+
+            $item->setState(1, false);
+            $item->publish_down = $jbdate->toMysql($oldDate + $addTime);
+        }
+
+        $this->set('is_modified', self::MODIFIED_YES);
+        $this->set('modified', $jbdate->toMysql(time()));
+        if ($order && $order->id) {
+            $this->set('order_id', $order->id);
+        }
+
+        $this->app->table->item->save($item);
+
+        return true;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function _renderModifierParams()
+    {
+        $htmlParams = array();
+        $mParams    = $this->get('params', array());
+
+        foreach ($mParams as $key => $value) {
+
+            if ($key == 'item_element_value') {
+                //$value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+                //$value = '<pre>' . $value . '</pre>';
+                continue;
+
+            } else if ($key == 'item_php_eval') {
+                //$value = '<pre>' . $value . '</pre>';
+                continue;
+
+            } else if ($key == 'item_element_id') {
+                if ($element = $this->getItem()->getElement($value)) {
+                    $value = $element->config->get('name') ? $element->config->get('name') : $element->config->get('type');
+                }
+
+            } else if ($key == 'item_category_value') {
+
+                if ($application = $this->app->table->application->get($value['app'])) {
+                    $htmlParams['JBZOO_JBADVERT_EDIT_APP'] = $application->name;
+                }
+
+                if ($value['category'] == 0) {
+                    $htmlParams['JBZOO_JBADVERT_EDIT_CATEGORY'] = JText::_('JBZOO_JBADVERT_EDIT_CATEGORY_ROOT');
+                } else if ($category = $this->app->table->category->get($value['category'])) {
+                    $htmlParams['JBZOO_JBADVERT_EDIT_CATEGORY'] = $category->name;
+                }
+
+            }
+
+            if (!empty($value) && !is_array($value)) {
+                $htmlParams['JBZOO_JBADVERT_EDIT_' . $key] = $value;
             }
         }
 
-        return $values;
+        return $this->app->jbhtml->dataList($htmlParams);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _isModified()
+    {
+        return (int)$this->get('is_modified', self::MODIFIED_NO) == self::MODIFIED_YES;
+    }
+
+    /**
+     * @return array
+     */
+    protected function _getModifiersParams()
+    {
+        $mode = $this->_getMode();
+
+        $result = array();
+        foreach ($this->config as $key => $value) {
+            if (strpos($key, 'item_' . $mode) === 0) {
+                $result[$key] = $value;
+            }
+        }
+
+        return $result;
     }
 
     /**
@@ -145,155 +322,143 @@ class ElementJBAdvert extends Element implements iSubmittable
      */
     public function addToCart()
     {
-        $cart = JBCart::getInstance();
-        $data = $this->getData();
-
-        $cart->addItem($data);
-
+        JBCart::getInstance()->addItem($this->_getCartData());
         return $this;
     }
 
     /**
-     * @param bool $refresh
      * @return array
      */
-    public function getData($refresh = false)
+    protected function _getCartData()
     {
-        static $data;
+        $price = $this->_getPrice();
+        $item  = $this->getItem();
 
-        if (!isset($data) || $refresh === true) {
-            $data = array(
-                'key'        => $this->getSessionKey(),
-                'item_id'    => $this->getItem()->id,
-                'item_name'  => $this->getItem()->name,
-                'element_id' => $this->identifier,
-                'total'      => $this->getTotal()->data(true),
-                'quantity'   => 1,
-                'values'     => $this->getValues(),
-                'elements'   => array(
-                    '_value' => $this->getTotal()->data(true)
-                ),
-                'params'     => array(
-                    '_quantity' => array(
-                        'min'      => 1,
-                        'max'      => 1,
-                        'step'     => 1,
-                        'default'  => 1,
-                        'decimals' => 0
-                    )
-                ),
-                'variant'    => 0
-            );
+        $data = array(
+            'key'        => md5($this->identifier . serialize($this->_getModifiersParams())),
+            'item_id'    => $item->id,
+            'item_name'  => $item->name,
+            'advert_id'  => $this->identifier,
+            'element_id' => $this->identifier,
+            'total'      => $price->data(true),
+            'values'     => $this->_getCartValues(),
+            'quantity'   => 1,
+            'variant'    => 0,
+            'elements'   => array(
+                '_value' => $price->data(true)
+            ),
+            'params'     => array(
+                '_quantity' => array(
+                    'min'      => 1,
+                    'max'      => 1,
+                    'step'     => 1,
+                    'default'  => 1,
+                    'decimals' => 0
+                )
+            ),
+        );
 
-            $data = $this->app->data->create($data);
-        }
+        $data = $this->app->data->create($data);
 
         return $data;
+    }
+
+    /**
+     * @return array
+     */
+    protected function _getCartValues()
+    {
+        $modName     = JText::_('JBZOO_JBADVERT_MODE_' . $this->_getMode());
+        $elementName = $this->config->get('name', $modName);
+
+        return array(
+            JText::_('JBZOO_JBADVERT_CART_MODE') => $elementName ? $elementName : $modName,
+        );
+    }
+
+    /**
+     * Hack! Compatible with JBCart API
+     * @param $key
+     */
+    public function setDefault($key)
+    {
+        //$this->set('default_variant', $key);
+    }
+
+    /**
+     * Hack! Compatible with JBCart API
+     */
+    public function inStock()
+    {
+        return 1;
     }
 
     /**
      * Get total sum
      * @return JBCartValue
      */
-    public function getTotal()
+    protected function _getPrice()
     {
-        $prices = $this->config->get('prices');
-        $total  = JBCart::val();
+        $price = $this->config->get('price', 0);
+        $order = $this->_getRelatedOrder();
 
-        if (!empty($prices)) {
-            foreach ($prices as $price) {
-                $total->add($price);
-            }
+        $result = JBCart::val($price);
+        if ($order && $order->id) {
+            $result = $order->val($price);
         }
 
-        return $total;
+        return $result;
     }
 
     /**
-     * @return array
+     * @return mixed
      */
-    public function getValues()
+    protected function _getMode()
     {
-        $data   = (array)$this->data();
-        $values = array();
-
-        if (!empty($data)) {
-            $ok = JText::_('JBZOO_YES');
-            foreach ($this->data as $key => $value) {
-                $values[$value] = $ok;
-            }
-        }
-
-        return $values;
+        return $this->config->get('mode', self::MODE_PUBLISH);
     }
 
     /**
-     * Get session key
+     * @param bool $isLink
+     * @return JBCartOrder|string
+     */
+    protected function _getRelatedOrder($isLink = false)
+    {
+        $order = JBModelOrder::model()->getById($this->get('order_id', 0));
+
+        if ($isLink) {
+            $orderLink = JText::sprintf('JBZOO_JBDVERT_ORDER_UNDEFINED', $this->get('order_id', 0));
+            if ($order && $order->id) {
+                $orderLink = $order->getUrl(null, 'full');
+            }
+
+            return $orderLink;
+        }
+
+        return $order;
+    }
+
+    /**
+     * @return $this
+     */
+    public function loadAssets()
+    {
+        $this->app->jbassets->less('elements:jbadvert/assets/less/jbadvert.less');
+        $this->app->jbassets->js('elements:jbadvert/assets/js/jbadvert.js');
+
+        return parent::loadAssets();
+    }
+
+    /**
      * @return string
      */
-    public function getSessionKey()
+    protected function _getSaveKey()
     {
-        return md5($this->identifier . implode($this->getValues()));
-    }
-
-    /**
-     * @return int
-     * @internal param int $key Number of variant
-     */
-    public function getBalance()
-    {
-        return 1;
-    }
-
-    /**
-     * Get parameter form object to render input form.
-     * @return Object
-     */
-    public function getConfigForm()
-    {
-        return parent::getConfigForm()->addElementPath(dirname(__FILE__));
-    }
-
-    /**
-     * @param  integer $quantity
-     * @return bool
-     */
-    public function inStock($quantity)
-    {
-        $quantity = (int)$quantity;
-
-        if ($quantity != $this->getBalance()) {
-            return false;
+        if ($item = $this->getItem()) {
+            return $this->_sesKey = 'data-' . $item->id . '-' . $this->identifier;
         }
 
-        return (bool)$quantity;
+        return $this->identifier;
     }
 
-    /**
-     * @param int $key
-     */
-    public function setDefault($key)
-    {
-        $this->set('default_variant', $key);
-    }
-
-    /**
-     * Get services with prices
-     * @param array $data Array of services
-     * @return array
-     */
-    public function addPrices($data)
-    {
-        $config = $this->config;
-        if (!empty($data)) {
-            foreach ($data as $key => $value) {
-                $price = $config->find('prices.' . $key, 0);
-
-                $data[$key] .= ' <small>(' . JBCart::val($price)->text() . ')</small>';
-
-            }
-        }
-
-        return $data;
-    }
 }
--- packages/jbuniversal/jbuniversal/elements/jbadvert/tmpl/edit.php
+++ packages/jbuniversal/jbuniversal/elements/jbadvert/tmpl/edit.php
@@ -12,5 +12,34 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-// Services
-echo $this->app->jbhtml->checkbox($data, $this->getControlName('value', true), null, $this->data());
+
+$elemId = $this->app->jbstring->getId('jbadvert');
+
+$toggleList = array(
+    0 => JText::_('JBZOO_NO'),
+    1 => JText::_('JBZOO_YES'),
+    2 => JText::_('JBZOO_JBADVERT_EDIT_IS_MODIFIED_EXEC'),
+);
+
+$mode = $this->_getMode();
+if ($mode == ElementJBAdvert::MODE_CATEGORY) {
+    unset($toggleList[2]);
+}
+
+$toggler = $this->app->jbhtml->radio($toggleList, $this->getControlName('is_modified'), array(), $this->get('is_modified', 0));
+
+echo '<div class="jbzoo"><div id="' . $elemId . '" class="jbadvert-edit">';
+echo $this->app->jbhtml->dataList(array(
+    'JBZOO_JBADVERT_EDIT_IS_MODIFIED' => $toggler,
+    'JBZOO_JBADVERT_EDIT_PRICE'       => JBCart::val($this->get('price', 0)),
+    'JBZOO_JBADVERT_EDIT_ORDER'       => $this->_getRelatedOrder(true),
+    'JBZOO_JBADVERT_EDIT_MODIFIED'    => $this->_getLastModified(),
+    'JBZOO_JBADVERT_EDIT_MODE'        => JText::_('JBZOO_JBADVERT_MODE_' . $this->_getMode()),
+    'JBZOO_JBADVERT_EDIT_PARAMS'      => $this->_renderModifierParams(),
+));
+echo '</div></div>';
+
+$this->loadAssets();
+$this->app->jbassets->widget('#' . $elemId, 'JBZoo.JBAdvert', array(
+    'text_exec_alert' => JText::_('JBZOO_JBADVERT_EXEC_ALERT')
+));
