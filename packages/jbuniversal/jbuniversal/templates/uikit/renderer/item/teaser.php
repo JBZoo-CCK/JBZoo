94f381ad56d4e1cd13d86827a983354ede71f3d9
Sergey <kalistratov.s.m@gmail.com>
2015-06-03 12:23:04 +0000

    # Скромный uikit teaser по умолчанию

--- packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/teaser.php
+++ packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/teaser.php
@@ -20,28 +20,35 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
     <h4 class="item-title"><?php echo $this->renderPosition('title'); ?></h4>
 <?php endif; ?>
 
-<?php if ($this->checkPosition('image')) : ?>
-    <div class="item-image uk-align-<?php echo $align; ?>">
-        <?php echo $this->renderPosition('image'); ?>
-    </div>
-<?php endif; ?>
+<div class="uk-grid">
+    <?php if ($this->checkPosition('image')) : ?>
+        <div class="uk-width-medium-1-2">
+            <div class="item-image uk-align-<?php echo $align; ?>">
+                <?php echo $this->renderPosition('image'); ?>
+            </div>
+        </div>
+    <?php endif; ?>
 
-<?php if ($this->checkPosition('properties')) : ?>
-    <div class="item-properties">
-        <ul class="uk-list uk-list-line">
-            <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
-        </ul>
-    </div>
-<?php endif; ?>
+    <?php if ($this->checkPosition('properties')) : ?>
+        <div class="uk-width-medium-1-2">
+            <div class="item-properties">
+                <ul class="uk-list uk-list-line">
+                    <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
+                </ul>
+            </div>
+        </div>
+    <?php endif; ?>
+</div>
 
 <?php if ($this->checkPosition('text')) : ?>
-    <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
-<?php endif; ?>
-
-<?php if ($this->checkPosition('meta')) : ?>
-    <div class="item-metadata">
-        <ul class="uk-list uk-list-line">
-            <?php echo $this->renderPosition('meta', array('style' => 'list')); ?>
-        </ul>
+    <div class="item-text uk-article-divider">
+        <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
     </div>
 <?php endif; ?>
+
+<?php if ($this->checkPosition('buttons')) : ?>
+    <div class="item-buttons">
+        <?php echo $this->renderPosition('buttons'); ?>
+    </div>
+<?php endif; ?>
+
