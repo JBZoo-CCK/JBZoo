15e8c4ff9bbc7ddb5f6e2933c2663eea96e1e5f0
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-03 12:30:53 +0000

    # Uikit кнопка для правки материала с фронта

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/item.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/item.php
@@ -23,9 +23,12 @@ $layout = $this->app->jbrequest->get('jbquickview');
 if (method_exists($this->item, 'canEdit') && $this->item->canEdit()) {
     $edit_link = $this->app->route->submission($this->item->getApplication()->getItemEditSubmission(), $this->item->type, null, $this->item->id, 'itemedit');
     ?>
-    <div class="item-edit-link">
+    <div class="item-edit-link uk-margin-bottom">
         <a href="<?php echo JRoute::_($edit_link); ?>" title="<?php echo JText::_('Edit Item'); ?>"
-           class="item-icon edit-item"><?php echo JText::_('Edit Item'); ?></a>
+           class="uk-button uk-button-primary edit-item">
+            <i class="uk-icon-edit"></i>
+            <?php echo JText::_('Edit Item'); ?>
+        </a>
     </div>
 <?php }
 
