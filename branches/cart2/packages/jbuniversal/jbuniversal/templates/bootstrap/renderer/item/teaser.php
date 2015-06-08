5b07c72dff834076f15444d5c006b47f147f3369
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-08 06:52:34 +0000

    # BOOTSTRAP: Стили, позиции для teaser

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/item/teaser.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/item/teaser.php
@@ -13,26 +13,25 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-$align = $this->app->jbitem->getMediaAlign($item, $layout);
 ?>
 
 <?php if ($this->checkPosition('title')) : ?>
     <h4 class="item-title"><?php echo $this->renderPosition('title'); ?></h4>
 <?php endif; ?>
 
-<div class="uk-grid">
+<div class="row">
     <?php if ($this->checkPosition('image')) : ?>
-        <div class="uk-width-medium-1-2">
-            <div class="item-image uk-align-<?php echo $align; ?>">
+        <div class="col-md-6">
+            <div class="item-image">
                 <?php echo $this->renderPosition('image'); ?>
             </div>
         </div>
     <?php endif; ?>
 
     <?php if ($this->checkPosition('properties')) : ?>
-        <div class="uk-width-medium-1-2">
+        <div class="col-md-6">
             <div class="item-properties">
-                <ul class="uk-list uk-list-line">
+                <ul class="list-unstyled">
                     <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
                 </ul>
             </div>
@@ -41,14 +40,13 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
 </div>
 
 <?php if ($this->checkPosition('text')) : ?>
-    <div class="item-text uk-article-divider">
+    <div class="item-text row">
         <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
     </div>
 <?php endif; ?>
 
 <?php if ($this->checkPosition('buttons')) : ?>
-    <div class="item-buttons">
+    <div class="item-buttons clearfix">
         <?php echo $this->renderPosition('buttons'); ?>
     </div>
 <?php endif; ?>
-
