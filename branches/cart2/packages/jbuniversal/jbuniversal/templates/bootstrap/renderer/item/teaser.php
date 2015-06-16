0e9511372bb9608c80e43e196c73ec06e0965dc0
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-16 20:44:59 +0000

    # BOOTSTRAP: Совместимый с версиями фрейма шаблон teaser

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/item/teaser.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/item/teaser.php
@@ -13,22 +13,24 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+$bootstrap = $this->app->jbbootstrap;
+$rowClass  = $bootstrap->getRowClass();
 ?>
 
 <?php if ($this->checkPosition('title')) : ?>
     <h4 class="item-title"><?php echo $this->renderPosition('title'); ?></h4>
 <?php endif; ?>
 
-<div class="row">
+<div class="<?php echo $rowClass; ?>">
     <?php if ($this->checkPosition('image')) : ?>
-        <div class="col-md-6">
+        <div class="<?php echo $bootstrap->gridClass(6); ?>">
             <div class="item-image">
                 <?php echo $this->renderPosition('image'); ?>
             </div>
         </div>
     <?php endif; ?>
 
-    <div class="col-md-6">
+    <div class="<?php echo $bootstrap->gridClass(6); ?>">
         <?php if ($this->checkPosition('properties')) : ?>
             <div class="item-properties">
                 <ul class="list-unstyled">
@@ -46,24 +48,24 @@ defined('_JEXEC') or die('Restricted access');
 </div>
 
 <?php if ($this->checkPosition('text')) : ?>
-    <div class="item-text row">
-        <div class="col-md-12">
+    <div class="item-text <?php echo $rowClass; ?>">
+        <div class="<?php echo $bootstrap->gridClass(12); ?>">
             <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
         </div>
     </div>
 <?php endif; ?>
 
 <?php if ($this->checkPosition('quick-view')) : ?>
-    <div class="item-quick-view row">
-        <div class="col-md-12">
+    <div class="item-quick-view <?php echo $rowClass; ?>">
+        <div class="<?php echo $bootstrap->gridClass(12); ?>">
             <?php echo $this->renderPosition('quick-view', array('style' => 'block')); ?>
         </div>
     </div>
 <?php endif; ?>
 
 <?php if ($this->checkPosition('buttons')) : ?>
-    <div class="row item-buttons clearfix">
-        <div class="col-md-12">
+    <div class="<?php echo $rowClass; ?> item-buttons clearfix">
+        <div class="<?php echo $bootstrap->gridClass(12); ?>">
             <?php echo $this->renderPosition('buttons'); ?>
         </div>
     </div>
