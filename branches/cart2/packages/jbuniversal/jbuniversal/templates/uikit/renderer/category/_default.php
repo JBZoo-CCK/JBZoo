003899cb985953524eef140f0ac5102514730fb0
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-21 14:11:05 +0000

    # UIKIT: Верстка вывода категории (были не нужные отступы)

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/category/_default.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/category/_default.php
@@ -18,53 +18,62 @@ $this->app->jbdebug->mark('layout::category::start');
 
 // set vars
 $category = $vars['object'];
-$title = $this->app->string->trim($vars['params']->get('content.category_title', ''));
+$title    = $this->app->string->trim($vars['params']->get('content.category_title', ''));
 $subTitle = $this->app->string->trim($vars['params']->get('content.category_subtitle', ''));
-$image = $this->app->jbimage->get('category_image', $vars['params']);
-
-$title = $title ? $title : $category->name;
+$image    = $this->app->jbimage->get('category_image', $vars['params']);
+$title    = $title ? $title : $category->name;
 
 if ((int)$vars['params']->get('template.category_show', 1)) : ?>
-    <div class="category alias-<?php echo $category->alias; ?> uk-panel uk-panel-box uk-article-divider">
+    <div class="category alias-<?php echo $category->alias; ?> uk-article-divider uk-grid">
 
-        <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
-            <h1 class="title"><?php echo $title; ?></h1>
-        <?php endif; ?>
-
-
-        <?php if ((int)$vars['params']->get('template.category_subtitle', 1) && !empty($subTitle)) : ?>
-            <h2 class="subtitle"><?php echo $subTitle; ?></h2>
-        <?php endif; ?>
+        <div class="uk-width-medium-1-1">
+            <div class="uk-panel uk-panel-box">
+                <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
+                    <h1 class="title"><?php echo $title; ?></h1>
+                <?php endif; ?>
 
 
-        <?php if ((int)$vars['params']->get('template.category_image', 1) && $image['src']) : ?>
-            <div class="image-full uk-align-<?php echo $vars['params']->get('template.category_image_align', 'left'); ?>">
-                <img src="<?php echo $image['src']; ?>" <?php echo $image['width_height']; ?>
-                     title="<?php echo $category->name; ?>" alt="<?php echo $category->name; ?>" class="uk-thumbnail"/>
+                <?php if ((int)$vars['params']->get('template.category_subtitle', 1) && !empty($subTitle)) : ?>
+                    <h2 class="subtitle"><?php echo $subTitle; ?></h2>
+                <?php endif; ?>
+
+
+                <?php if ((int)$vars['params']->get('template.category_image', 1) && $image['src']) : ?>
+                    <div class="image-full uk-align-<?php echo $vars['params']->get('template.category_image_align', 'left'); ?>">
+                        <img src="<?php echo $image['src']; ?>" <?php echo $image['width_height']; ?>
+                             title="<?php echo $category->name; ?>" alt="<?php echo $category->name; ?>" class="uk-thumbnail"/>
+                    </div>
+                <?php endif; ?>
+
+
+                <?php if ((int)$vars['params']->get('template.category_teaser_text', 1) && $vars['params']->get('content.category_teaser_text', '')) : ?>
+                    <div class="description-teaser">
+                        <?php echo $vars['params']->get('content.category_teaser_text', ''); ?>
+                    </div>
+                <?php endif; ?>
+
+
+                <?php if ((int)$vars['params']->get('template.category_text', 1) && $category->description) : ?>
+                    <div class="description-full"><?php echo $category->getText($category->description); ?></div>
+                <?php endif; ?>
+
+
+                <?php echo JBZOO_CLR; ?>
             </div>
-        <?php endif; ?>
 
+        </div>
 
-        <?php if ((int)$vars['params']->get('template.category_teaser_text', 1) && $vars['params']->get('content.category_teaser_text', '')) : ?>
-            <div class="description-teaser">
-                <?php echo $vars['params']->get('content.category_teaser_text', ''); ?>
-            </div>
-        <?php endif; ?>
-
-
-        <?php if ((int)$vars['params']->get('template.category_text', 1) && $category->description) : ?>
-            <div class="description-full"><?php echo $category->getText($category->description); ?></div>
-        <?php endif; ?>
-
-
-        <?php echo JBZOO_CLR; ?>
     </div>
 
 <?php else: ?>
 
-    <div class="category alias-<?php echo $category->alias; ?> uk-panel uk-panel-box uk-article-divider">
+    <div class="category alias-<?php echo $category->alias; ?> uk-article-divider uk-grid">
         <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
-            <h1 class="title"><?php echo $title; ?></h1>
+            <div class="uk-width-medium-1-1">
+                <div class="uk-panel uk-panel-box">
+                    <h1 class="title"><?php echo $title; ?></h1>
+                </div>
+            </div>
         <?php endif; ?>
     </div>
 
--- branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/teaser.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/teaser.php
@@ -51,4 +51,3 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
         <?php echo $this->renderPosition('buttons'); ?>
     </div>
 <?php endif; ?>
-
