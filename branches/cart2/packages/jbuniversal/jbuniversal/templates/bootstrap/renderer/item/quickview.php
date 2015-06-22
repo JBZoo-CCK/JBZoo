0cc3dee6694f2fa7a7fa27d1a61a7a9d19529987
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-22 13:44:26 +0000

    # BOOTSTRAP: шаблон quickview.php

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/item/quickview.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/item/quickview.php
@@ -22,22 +22,16 @@ $rowClass  = $bootstrap->getRowClass();
 
 ?>
 
-<?php if ($this->checkPosition('top')) : ?>
-    <div class="item-top">
-        <?php echo $this->renderPosition('top', array('style' => 'block')); ?>
-    </div>
+<?php if ($this->checkPosition('title')) : ?>
+    <h1 class="item-title"><?php echo $this->renderPosition('title'); ?></h1>
 <?php endif; ?>
 
-<div class="item-body clearfix">
+<div class="well item-body">
     <div class="head-wrapper">
         <?php if ($this->checkPosition('image')) : ?>
             <div class="item-image pull-<?php echo $align; ?>">
                 <?php echo $this->renderPosition('image'); ?>
             </div>
-        <?php endif; ?>
-
-        <?php if ($this->checkPosition('title')) : ?>
-            <h1 class="item-title"><?php echo $this->renderPosition('title'); ?></h1>
         <?php endif; ?>
 
         <?php if ($this->checkPosition('description')) : ?>
@@ -47,40 +41,27 @@ $rowClass  = $bootstrap->getRowClass();
         <?php endif; ?>
     </div>
 
-    <div class="<?php echo $rowClass; ?> clearfix">
-        <?php if ($this->checkPosition('price')) : ?>
-            <div class="<?php echo $bootstrap->gridClass(2); ?> item-price jsCartModal">
-                <?php echo $this->renderPosition('price'); ?>
-            </div>
-        <?php endif; ?>
-
-        <?php if ($this->checkPosition('properties')) : ?>
-            <div class="<?php echo $bootstrap->gridClass(2); ?> item-properties">
-                <h3><?php echo JText::_('JBZOO_QUICKVIEW_SPECIFICATION'); ?></h3>
-                <table class="table table-hover">
-                    <?php echo $this->renderPosition('properties', array('style' => 'jbtable')); ?>
-                </table>
-            </div>
-        <?php endif; ?>
-    </div>
-
-    <?php if ($this->checkPosition('bottom')) : ?>
-        <div class="<?php echo $rowClass; ?> item-bottom">
-            <div class="<?php echo $bootstrap->gridClass(12); ?>">
-                <?php echo $this->renderPosition('bottom', array('style' => 'block')); ?>
-            </div>
+    <?php if ($this->checkPosition('properties')) : ?>
+        <div class="item-properties">
+            <h3><?php echo JText::_('JBZOO_QUICKVIEW_SPECIFICATION'); ?></h3>
+            <table class="table table-hover">
+                <?php echo $this->renderPosition('properties', array(
+                    'tooltip' => true,
+                    'style'   => 'jbtable',
+                )); ?>
+            </table>
         </div>
     <?php endif; ?>
 
-    <?php if ($this->checkPosition('related')) : ?>
-        <div class="<?php echo $rowClass; ?> item-related">
-            <div class="<?php echo $bootstrap->gridClass(12); ?>">
-                <?php echo $this->renderPosition('related', array(
-                    'labelTag' => 'h4',
-                    'style'    => 'jbblock',
-                )); ?>
-            </div>
+    <?php if ($this->checkPosition('price')) : ?>
+        <div class="item-price jsCartModal">
+            <?php echo $this->renderPosition('price'); ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if ($this->checkPosition('bottom')) : ?>
+        <div class="item-bottom clearfix">
+            <?php echo $this->renderPosition('bottom', array('style' => 'block')); ?>
         </div>
     <?php endif; ?>
 </div>
-
