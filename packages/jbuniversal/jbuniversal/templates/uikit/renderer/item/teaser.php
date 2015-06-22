2548d80387e92c23e5a8ee08d03d2d7e6a53b421
Sergey <kalistratov.s.m@gmail.com>
2015-06-22 10:58:16 +0000

    # UIKIT: фикс шаблона subcategory + Стили и позиции для стандарта шаблона teaser

--- packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/teaser.php
+++ packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/teaser.php
@@ -20,34 +20,42 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
     <h4 class="item-title"><?php echo $this->renderPosition('title'); ?></h4>
 <?php endif; ?>
 
-<div class="uk-grid">
+<div class="uk-clearfix">
     <?php if ($this->checkPosition('image')) : ?>
-        <div class="uk-width-medium-1-2">
-            <div class="item-image uk-align-<?php echo $align; ?>">
-                <?php echo $this->renderPosition('image'); ?>
-            </div>
+        <div class="item-image uk-align-<?php echo $align; ?>">
+            <?php echo $this->renderPosition('image'); ?>
         </div>
     <?php endif; ?>
 
     <?php if ($this->checkPosition('properties')) : ?>
-        <div class="uk-width-medium-1-2">
-            <div class="item-properties">
-                <ul class="uk-list uk-list-line">
-                    <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
-                </ul>
-            </div>
+        <div class="item-properties">
+            <ul class="uk-list">
+                <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
+            </ul>
         </div>
     <?php endif; ?>
 </div>
 
+<?php if ($this->checkPosition('price')) : ?>
+    <div class="item-price uk-divider">
+        <?php echo $this->renderPosition('price', array('style' => 'block')); ?>
+    </div>
+<?php endif; ?>
+
 <?php if ($this->checkPosition('text')) : ?>
-    <div class="item-text uk-article-divider">
+    <div class="item-text uk-divider">
         <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
     </div>
 <?php endif; ?>
 
+<?php if ($this->checkPosition('quick-view')) : ?>
+    <div class="item-quick-view uk-divider">
+        <?php echo $this->renderPosition('quick-view', array('style' => 'block')); ?>
+    </div>
+<?php endif; ?>
+
 <?php if ($this->checkPosition('buttons')) : ?>
-    <div class="item-buttons">
+    <div class="item-buttons uk-divider">
         <?php echo $this->renderPosition('buttons'); ?>
     </div>
 <?php endif; ?>
