f35acafe88f05046d6583c6b5f0b8c2a2d8cff4d
Sergey <kalistratov.s.m@gmail.com>
2015-06-22 11:05:01 +0000

    # UIKIT: атрибуты title для элементов compare & favorite

--- packages/jbuniversal/jbuniversal/templates/bootstrap/elements/jbcompare/tmpl/jbcompare.php
+++ packages/jbuniversal/jbuniversal/templates/bootstrap/elements/jbcompare/tmpl/jbcompare.php
@@ -30,15 +30,15 @@ $wrapAttrs = array(
 <!--noindex-->
 <div <?php echo $this->app->jbhtml->buildAttrs($wrapAttrs); ?>>
     <div class="jbcompare-active">
-        <a rel="nofollow" href="<?php echo $compareUrl; ?>" class="btn btn-primary" title="<?php echo JText::_('JBZOO_FAVORITE_ITEMS'); ?>">
+        <a rel="nofollow" href="<?php echo $compareUrl; ?>" class="btn btn-primary" title="<?php echo JText::_('JBZOO_COMPARE_ITEMS'); ?>">
             <?php echo $this->app->jbbootstrap->icon('thumbs-up', array('type' => 'white')); ?>
         </a>
-        <span class="btn btn-danger jsCompareToggle" title="<?php echo JText::_('JBZOO_FAVORITE_REMOVE'); ?>">
+        <span class="btn btn-danger jsCompareToggle" title="<?php echo JText::_('JBZOO_COMPARE_REMOVE'); ?>">
             <?php echo $this->app->jbbootstrap->icon('trash', array('type' => 'white')); ?>
         </span>
     </div>
     <div class="jbcompare-unactive">
-        <span class="btn btn-primary jsCompareToggle" title="<?php echo JText::_('JBZOO_FAVORITE_ADD'); ?>">
+        <span class="btn btn-primary jsCompareToggle" title="<?php echo JText::_('JBZOO_COMPARE_ADD'); ?>">
             <?php echo $this->app->jbbootstrap->icon('thumbs-up', array('type' => 'white')); ?>
         </span>
     </div>
