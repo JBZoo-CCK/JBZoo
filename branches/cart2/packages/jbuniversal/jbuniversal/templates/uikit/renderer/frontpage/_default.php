2a25166a0867431d65d9ff931d3761a79af16d19
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-22 10:58:16 +0000

    # UIKIT: фикс шаблона subcategory + Стили и позиции для стандарта шаблона teaser

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/frontpage/_default.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/frontpage/_default.php
@@ -24,45 +24,48 @@ $image    = $this->app->jbimage->get('category_image', $vars['params']);
 $title    = $title ? $title : $category->name;
 
 if ((int)$vars['params']->get('template.category_show', 1)) : ?>
-    <div class="frontpage clearfix rborder alias-<?php echo $category->alias; ?> uk-panel uk-panel-box">
-
-        <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
-            <h1 class="title"><?php echo $title; ?></h1>
-        <?php endif; ?>
-
-
-        <?php if ((int)$vars['params']->get('template.category_subtitle', 1) && !empty($subTitle)) : ?>
-            <h2 class="subtitle"><?php echo $subTitle; ?></h2>
-        <?php endif; ?>
+    <div class="frontpage clearfix alias-<?php echo $category->alias; ?> uk-grid">
+        <div class="uk-width-medium-1-1">
+            <div class="uk-panel uk-panel-box">
+                <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
+                    <h1 class="title"><?php echo $title; ?></h1>
+                <?php endif; ?>
 
 
-        <?php if ((int)$vars['params']->get('template.category_image', 1) && $image['src']) : ?>
-            <div class="image-full uk-align-<?php echo $vars['params']->get('template.category_image_align', 'left'); ?>">
-                <img src="<?php echo $image['src']; ?>" <?php echo $image['width_height']; ?>
-                     title="<?php echo $category->name; ?>" alt="<?php echo $category->name; ?>" class="uk-thumbnail"/>
+                <?php if ((int)$vars['params']->get('template.category_subtitle', 1) && !empty($subTitle)) : ?>
+                    <h2 class="subtitle"><?php echo $subTitle; ?></h2>
+                <?php endif; ?>
+
+
+                <?php if ((int)$vars['params']->get('template.category_image', 1) && $image['src']) : ?>
+                    <div class="image-full uk-align-<?php echo $vars['params']->get('template.category_image_align', 'left'); ?>">
+                        <img src="<?php echo $image['src']; ?>" <?php echo $image['width_height']; ?>
+                             title="<?php echo $category->name; ?>" alt="<?php echo $category->name; ?>" class="uk-thumbnail"/>
+                    </div>
+                <?php endif; ?>
+
+
+                <?php if ((int)$vars['params']->get('template.category_teaser_text', 1) && $vars['params']->get('content.category_teaser_text', '')) : ?>
+                    <div class="description-teaser">
+                        <?php echo $vars['params']->get('content.category_teaser_text', ''); ?>
+                    </div>
+                <?php endif; ?>
+
+
+                <?php if ((int)$vars['params']->get('template.category_text', 1) && $category->description) : ?>
+                    <div class="description-full"><?php echo $category->getText($category->description); ?></div>
+                <?php endif; ?>
             </div>
-        <?php endif; ?>
-
-
-        <?php if ((int)$vars['params']->get('template.category_teaser_text', 1) && $vars['params']->get('content.category_teaser_text', '')) : ?>
-            <div class="description-teaser">
-                <?php echo $vars['params']->get('content.category_teaser_text', ''); ?>
-            </div>
-        <?php endif; ?>
-
-
-        <?php if ((int)$vars['params']->get('template.category_text', 1) && $category->description) : ?>
-            <div class="description-full"><?php echo $category->getText($category->description); ?></div>
-        <?php endif; ?>
-
+        </div>
     </div>
-
 <?php else: ?>
 
-    <div class="frontpage alias-<?php echo $category->alias; ?> uk-panel uk-panel-box">
-        <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
-            <h1 class="title"><?php echo $title; ?></h1>
-        <?php endif; ?>
+    <div class="frontpage alias-<?php echo $category->alias; ?> uk-grid">
+        <div class="uk-width-medium-1-1">
+            <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
+                <h1 class="title"><?php echo $title; ?></h1>
+            <?php endif; ?>
+        </div>
     </div>
 
 <?php endif; ?>
