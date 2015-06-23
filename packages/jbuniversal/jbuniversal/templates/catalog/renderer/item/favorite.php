3716b077f596686aa2955d1ebe862850fe2a20a7
Sergey <kalistratov.s.m@gmail.com>
2015-06-23 11:20:52 +0000

    # CATALOG: Шаблон favorite

--- packages/jbuniversal/jbuniversal/templates/catalog/renderer/item/favorite.php
+++ packages/jbuniversal/jbuniversal/templates/catalog/renderer/item/favorite.php
@@ -18,33 +18,37 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
 ?>
 
 <?php if ($this->checkPosition('title')) : ?>
-    <h4 class="item-title"><?php echo $this->renderPosition('title'); ?></h4>
+    <h2 class="item-title"><?php echo $this->renderPosition('title'); ?></h2>
 <?php endif; ?>
 
+<div class="jb-row clearfix">
+    <div class="left-box">
+        <?php if ($this->checkPosition('image')) : ?>
+            <div class="item-image">
+                <?php echo $this->renderPosition('image'); ?>
+            </div>
+        <?php endif; ?>
 
-<?php if ($this->checkPosition('image')) : ?>
-    <div class="item-image align-<?php echo $align; ?>">
-        <?php echo $this->renderPosition('image'); ?>
+        <?php if ($this->checkPosition('price')) : ?>
+            <div class="item-price">
+                <?php echo $this->renderPosition('price', array('style' => 'jbblock')); ?>
+            </div>
+        <?php endif; ?>
     </div>
-<?php endif; ?>
+    <div class="right-box">
+        <?php if ($this->checkPosition('text')) : ?>
+            <div class="item-text">
+                <?php echo $this->renderPosition('text', array(
+                    'labelTag' => 'h4',
+                    'style'    => 'block',
+                )); ?>
+            </div>
+        <?php endif; ?>
 
-
-<?php if ($this->checkPosition('properties')) : ?>
-    <ul class="item-properties">
-        <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
-    </ul>
-<?php endif; ?>
-
-
-<?php if ($this->checkPosition('text')) : ?>
-    <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
-<?php endif; ?>
-
-
-<?php if ($this->checkPosition('meta')) : ?>
-    <ul class="item-metadata">
-        <?php echo $this->renderPosition('meta', array('style' => 'list')); ?>
-    </ul>
-<?php endif; ?>
-
-<?php echo JBZOO_CLR; ?>
+        <?php if ($this->checkPosition('properties')) : ?>
+            <table class="jbtable">
+                <?php echo $this->renderPosition('properties', array('style' => 'jbtable')); ?>
+            </table>
+        <?php endif; ?>
+    </div>
+</div>
