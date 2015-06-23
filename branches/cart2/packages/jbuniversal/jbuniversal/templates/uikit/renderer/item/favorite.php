18e0bd84d88a7acfb02ef88902509243ac668988
cheren <cheren@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-06-23 10:23:46 +0000

    # UIKIT: Шаблон favofite

--- branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/favorite.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/templates/uikit/renderer/item/favorite.php
@@ -21,26 +21,35 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
     <h2 class="item-title"><?php echo $this->renderPosition('title'); ?></h2>
 <?php endif; ?>
 
-<?php if ($this->checkPosition('image')) : ?>
-    <div class="item-image uk-align-<?php echo $align; ?>">
-        <?php echo $this->renderPosition('image'); ?>
+<div class="uk-grid">
+    <div class="uk-width-medium-3-10">
+        <?php if ($this->checkPosition('image')) : ?>
+            <div class="item-image">
+                <?php echo $this->renderPosition('image'); ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($this->checkPosition('price')) : ?>
+            <div class="item-price">
+                <?php echo $this->renderPosition('price', array('style' => 'jbblock')); ?>
+            </div>
+        <?php endif; ?>
     </div>
-<?php endif; ?>
+    <div class="uk-width-medium-7-10">
+        <?php if ($this->checkPosition('text')) : ?>
+            <div class="item-text">
+                <?php echo $this->renderPosition('text', array(
+                    'labelTag' => 'h4',
+                    'style'    => 'block',
+                )); ?>
+            </div>
+        <?php endif; ?>
 
-<?php if ($this->checkPosition('properties')) : ?>
-    <ul class="item-properties uk-list uk-list-line">
-        <?php echo $this->renderPosition('properties', array('style' => 'list')); ?>
-    </ul>
-<?php endif; ?>
-
-<?php if ($this->checkPosition('text')) : ?>
-    <?php echo $this->renderPosition('text', array('style' => 'block')); ?>
-<?php endif; ?>
-
-<?php if ($this->checkPosition('meta')) : ?>
-    <div class="uk-clearfix item-metadata">
-        <ul class="uk-list">
-            <?php echo $this->renderPosition('meta', array('style' => 'list')); ?>
-        </ul>
+        <?php if ($this->checkPosition('properties')) : ?>
+            <table class="uk-table uk-table-hover uk-table-striped item-properties">
+                <?php echo $this->renderPosition('properties', array('style' => 'jbtable')); ?>
+            </table>
+        <?php endif; ?>
     </div>
-<?php endif; ?>
+</div>
+
