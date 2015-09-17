78287ad75017b2c6fd077666e22c44e86d5ae626
Denis Smetannikov <denis@jbzoo.com>
2015-09-17 08:08:42 +0000

    Обратная совместимость

---
+++ packages/jbuniversal/jbuniversal/framework/render/filter/element.jbpriceadvance.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Denis Smetannikov <denis@jbzoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+
+/**
+ * Class JBFilterElementJBPriceAdvance
+ */
+class JBFilterElementJBPriceAdvance extends JBFilterElement
+{
+    /**
+     * Render HTML code for element
+     * @return string|null
+     */
+    public function html()
+    {
+        return 'Deprecated element!';
+    }
+
+}
