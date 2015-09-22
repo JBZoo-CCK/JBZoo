d45509739c9cd2bc2304ee649d5381343deb6d5f
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2015-09-22 07:46:38 +0000

    пропущенные копирайты

--- branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/order/option/options.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/cart-elements/order/option/options.php
@@ -1,13 +1,17 @@
 <?php
 /**
- * @package   com_zoo
- * @author    YOOtheme http://www.yootheme.com
- * @copyright Copyright (C) YOOtheme GmbH
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license-pro.php JBZoo Licence
+ * @coder       Denis Smetannikov <denis@jbzoo.com>
  */
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
+
 
 // get element from parent parameter form
 $element = $parent->element;
