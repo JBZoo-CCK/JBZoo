a117199fb899fead41d44d5fe378ef5c45ebf3ae
Sergey <kalistratov.s.m@gmail.com>
2015-10-20 16:43:07 +0000

    # checkPosition для для mod_jbzoo_search/renderer/item

--- packages/mod_jbzoo_search/renderer/item/table.php
+++ packages/mod_jbzoo_search/renderer/item/table.php
@@ -16,15 +16,31 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 <table class="jbfilter-table no-border">
     <tr>
-        <td><?php echo $this->renderPosition('cell_1_1', array('style' => 'filter.block')); ?></td>
-        <td><?php echo $this->renderPosition('cell_1_2', array('style' => 'filter.block')); ?></td>
-        <td><?php echo $this->renderPosition('cell_1_3', array('style' => 'filter.block')); ?></td>
-        <td><?php echo $this->renderPosition('cell_1_4', array('style' => 'filter.block')); ?></td>
+        <?php if ($this->checkPosition('cell_1_1')) : ?>
+            <td><?php echo $this->renderPosition('cell_1_1', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
+        <?php if ($this->checkPosition('cell_1_2')) : ?>
+            <td><?php echo $this->renderPosition('cell_1_2', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
+        <?php if ($this->checkPosition('cell_1_3')) : ?>
+            <td><?php echo $this->renderPosition('cell_1_3', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
+        <?php if ($this->checkPosition('cell_1_4')) : ?>
+            <td><?php echo $this->renderPosition('cell_1_4', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
     </tr>
     <tr>
-        <td><?php echo $this->renderPosition('cell_2_1', array('style' => 'filter.block')); ?></td>
-        <td><?php echo $this->renderPosition('cell_2_2', array('style' => 'filter.block')); ?></td>
-        <td><?php echo $this->renderPosition('cell_2_3', array('style' => 'filter.block')); ?></td>
-        <td><?php echo $this->renderPosition('cell_2_4', array('style' => 'filter.block')); ?></td>
+        <?php if ($this->checkPosition('cell_2_1')) : ?>
+            <td><?php echo $this->renderPosition('cell_2_1', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
+        <?php if ($this->checkPosition('cell_2_2')) : ?>
+            <td><?php echo $this->renderPosition('cell_2_2', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
+        <?php if ($this->checkPosition('cell_2_3')) : ?>
+            <td><?php echo $this->renderPosition('cell_2_3', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
+        <?php if ($this->checkPosition('cell_2_4')) : ?>
+            <td><?php echo $this->renderPosition('cell_2_4', array('style' => 'filter.block')); ?></td>
+        <?php endif; ?>
     </tr>
 </table>
