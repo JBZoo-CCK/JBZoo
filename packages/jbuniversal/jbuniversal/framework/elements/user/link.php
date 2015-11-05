1f1fbd09d521c21a53e46696f7edbe38e92bafb8
Denis Smetannikov <denis@jbzoo.com>
2015-11-05 11:02:04 +0000

    Импорт и экспорт ссылок с анотациями

--- packages/jbuniversal/jbuniversal/framework/elements/user/link.php
+++ packages/jbuniversal/jbuniversal/framework/elements/user/link.php
@@ -25,9 +25,46 @@ class JBCSVItemUserLink extends JBCSVItem
      */
     public function toCSV()
     {
+        $result = array();
+
         foreach ($this->_item->elements[$this->_identifier] as $key => $self) {
-            $this->_item->elements[$this->_identifier][$key] = isset($self['value']) ? array('value' => rtrim($self['value'], '/')) : null;
+
+            $value = isset($self['value']) ? trim($self['value']) : null;
+            $text  = isset($self['text']) ? trim($self['text']) : null;
+
+            if ($text) {
+                $result[$key] = $value . JBCSVItem::SEP_CELL . $text;
+            } else {
+                $result[$key] = $value;
+            }
         }
-        return parent::toCSV();
+
+        return $result;
+    }
+
+    /**
+     * @param      $value
+     * @param null $position
+     * @return Item|void
+     */
+    public function fromCSV($value, $position = null)
+    {
+        $data   = ($position == 1) ? array() : $data = $this->_element->data();
+
+        if (strpos($value, JBCSVItem::SEP_CELL) === false) {
+            $text = '';
+            $link = $value;
+        } else {
+            list($link, $text) = explode(JBCSVItem::SEP_CELL, $value);
+        }
+
+        $data[] = array(
+            'text'  => $this->_getString($text),
+            'value' => $this->_getString($link),
+        );
+
+        $this->_element->bindData($data);
+
+        return $this->_item;
     }
 }
