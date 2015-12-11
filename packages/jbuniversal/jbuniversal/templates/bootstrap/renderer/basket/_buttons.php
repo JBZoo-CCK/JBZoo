75e468777aa489192ba1549c991183a4287eb2ab
Denis Smetannikov <denis@jbzoo.com>
2015-12-11 05:39:22 +0000

    Опции для отключения кнопок в корзине

--- packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/basket/_buttons.php
+++ packages/jbuniversal/jbuniversal/templates/bootstrap/renderer/basket/_buttons.php
@@ -12,14 +12,19 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+$isCreate  = (int)$view->config->get('tmpl_button_create', 1);
+$isPayment = (int)$view->config->get('tmpl_button_payment', 1) && $view->payment;
+
 ?>
 
 <div class="text-center jbform-actions clearfix">
 
-    <input type="submit" name="create" value="<?php echo JText::_('JBZOO_CART_SUBMIT'); ?>"
-           class="btn btn-success btn-lg" />
+    <?php if ($isCreate) : ?>
+        <input type="submit" name="create" value="<?php echo JText::_('JBZOO_CART_SUBMIT'); ?>"
+               class="btn btn-success btn-lg" />
+    <?php endif; ?>
 
-    <?php if ($view->payment) : ?>
+    <?php if ($isPayment) : ?>
         <input type="submit" name="create-pay" value="<?php echo JText::_('JBZOO_CART_SUBMIT_AND_PAY'); ?>"
                class="btn btn-success btn-lg" />
     <?php endif; ?>
