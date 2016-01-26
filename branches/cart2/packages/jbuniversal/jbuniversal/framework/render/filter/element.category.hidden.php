6b45bebc45189e43a75f5f29d22b2a46683db174
smetdenis <smetdenis@31a16f5c-c45d-45c5-acf3-1366edb0126d>
2016-01-26 08:52:51 +0000

    Скрытое поле для категории может использовать произвольное значение.

--- branches/cart2/packages/jbuniversal/jbuniversal/framework/render/filter/element.category.hidden.php
+++ branches/cart2/packages/jbuniversal/jbuniversal/framework/render/filter/element.category.hidden.php
@@ -26,13 +26,17 @@ class JBFilterElementCategoryHidden extends JBFilterElementCategory
      */
     public function html()
     {
+        $value = (int)$this->_params->get('jbzoo_filter_default', 0);
+        if (!$value) {
+            $value = $this->app->jbrequest->getSystem('category', '');
+        }
+
         $this->_isMultiple = false;
         return $this->app->jbhtml->hidden(
             $this->_getName(),
-            $this->app->jbrequest->getSystem('category', ''),
+            $value,
             $this->_attrs,
             $this->_getId()
         );
     }
-
 }
