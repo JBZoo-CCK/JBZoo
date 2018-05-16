4e8f1eb8f738680d6666d9f649ddd04aa3568f99
CB9TOIIIA <cb9toiiia@gmail.com>
2018-05-16 11:58:38 +0300

    fix length search
    need more chars for search

--- packages/jbuniversal/jbuniversal/framework/controllers/autocomplete.php
+++ packages/jbuniversal/jbuniversal/framework/controllers/autocomplete.php
@@ -19,7 +19,7 @@ defined('_JEXEC') or die('Restricted access');
  */
 class AutocompleteJBUniversalController extends JBUniversalController
 {
-    const MAX_LENGTH = 20;
+    const MAX_LENGTH = 200;
 
     /**
      * @throws AppException
