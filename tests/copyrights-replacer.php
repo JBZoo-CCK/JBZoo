c5c550f9957b00aeeff8cb54cc2668c3d9f1bd61
SmetDenis <denis.smetannikov@item8.io>
2018-06-13 18:51:16 +0300

    GPL copyrights

--- tests/copyrights-replacer.php
+++ tests/copyrights-replacer.php
@@ -4,34 +4,28 @@ use Symfony\Component\Finder\Finder;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$validHeader = '<?php
-/**
- * JBZoo Application
- *
- * This file is part of the JBZoo CCK package.
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @package    Application
- * @license    GPL-2.0
- * @copyright  Copyright (C) JBZoo.com, All rights reserved.
- * @link       https://github.com/JBZoo/JBZoo
- * @author     Denis Smetannikov <denis@jbzoo.com>
- */
+$validHeader = '<?xml version="1.0" encoding="UTF-8" ?>
+<!--
+    JBZoo Application
 
-// no direct access
-defined(\'_JEXEC\') or die(\'Restricted access\');
+    This file is part of the JBZoo CCK package.
+    For the full copyright and license information, please view the LICENSE
+    file that was distributed with this source code.
 
-';
+    @package    Application
+    @license    GPL-2.0
+    @copyright  Copyright (C) JBZoo.com, All rights reserved.
+    @link       https://github.com/JBZoo/JBZoo
+-->';
 
 $finder = new Finder();
 $finder
     ->files()
     ->in(__DIR__ . '/../packages')
-    ->name('*.php');
+    ->name('*.xml');
 
-$rep = preg_quote("access');", null);
-$reg = "#(.*?{$rep}\s*)#ius";
+$rep = preg_quote('-->', null);
+$reg = "#(.*?{$rep})#ius";
 
 /** @var \SplFileInfo $file */
 foreach ($finder as $file) {
