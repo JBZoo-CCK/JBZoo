a6c80934b60d0f8ec30e7bc2b41e0c87ee08ee23
SmetDenis <denis.smetannikov@item8.io>
2018-07-21 14:03:44 +0300

    fix versions

--- src/packages/jbuniversal/jbuniversal/application.php
+++ src/packages/jbuniversal/jbuniversal/application.php
@@ -25,7 +25,7 @@ require_once __DIR__ . '/framework/jbzoo.php';
  */
 final class JBUniversalApplication extends Application
 {
-    const JBZOO_VERSION = '4.0.0';
+    const JBZOO_VERSION = '4.0.2';
 
     /**
      * @var bool
