2e25327891cb481df7e0cccc6da4b6fc6d36148f
Eugene Kopylov <cb9toiiia@gmail.com>
2018-11-23 23:58:01 +0300

    fix to correct autotest
    fix to correct autotest

--- src/packages/jbuniversal/jbuniversal/cart-elements/payment/yandexkassaemail/yandexkassaemail.php
+++ src/packages/jbuniversal/jbuniversal/cart-elements/payment/yandexkassaemail/yandexkassaemail.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * JBZoo Element YandexKassa by Email
+ * JBZoo Application
  *
  * This file is part of the JBZoo CCK package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package     JBZoo
- * @license     MIT
- * @copyright   Copyright (C) JBZoo.com, All rights reserved.
- * @link        https://github.com/JBZoo/Element-Payment-YandexKassa-Email
- * @author      Denis Smetannikov <denis@jbzoo.com>
+ * @package    Application
+ * @license    GPL-2.0
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @link       https://github.com/JBZoo/JBZoo
+ * @author     Denis Smetannikov <denis@jbzoo.com>
  */
 
 // no direct access
