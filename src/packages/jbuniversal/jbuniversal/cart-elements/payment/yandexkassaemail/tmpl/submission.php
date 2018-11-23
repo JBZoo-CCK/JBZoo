48721b928d3aa8cc355279cb33a53d20d65b48ce
Eugene Kopylov <cb9toiiia@gmail.com>
2018-11-23 23:53:43 +0300

    4.10.2 + ym_merchant_receipt
    4.10.2 + ym_merchant_receipt  + add kassa

---
+++ src/packages/jbuniversal/jbuniversal/cart-elements/payment/yandexkassaemail/tmpl/submission.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * JBZoo Element YandexKassa by Email
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package     JBZoo
+ * @license     MIT
+ * @copyright   Copyright (C) JBZoo.com, All rights reserved.
+ * @link        https://github.com/JBZoo/Element-Payment-YandexKassa-Email
+ * @author      Denis Smetannikov <denis@jbzoo.com>
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+
+$logo = JUri::root() . 'media/zoo/applications/jbuniversal/cart-elements/payment/yandexkassaemail/assets/logo.png';
+
+?>
+<p class="jbcart-payment-element-name">Оплата он-лайн</p>
+<img src="<?php echo $logo; ?>">
