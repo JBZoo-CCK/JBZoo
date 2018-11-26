7165e5611558d90f3199a5b0bf9c23a3adb6ecb0
Eugene Kopylov <cb9toiiia@gmail.com>
2018-11-27 00:35:59 +0300

    fix codestyle

--- src/packages/jbuniversal/jbuniversal/framework/helpers/jbfield.php
+++ src/packages/jbuniversal/jbuniversal/framework/helpers/jbfield.php
@@ -3,6 +3,7 @@
  * JBZoo Application
  *
  * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * @package    Application
