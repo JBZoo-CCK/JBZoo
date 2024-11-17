6cfc16f42b8ae68ef6f2d2157146561ce3204d2a
Eugene Kopylov <cb9toiiia@gmail.com>
2024-11-17 18:44:54 +0300

    fix cs

--- src/packages/jbuniversal/jbuniversal/elements/jbuploader/upload/index.php
+++ src/packages/jbuniversal/jbuniversal/elements/jbuploader/upload/index.php
@@ -1,12 +1,19 @@
 <?php
-
 /**
- * @package   FL Gallery Image Element for Zoo
- * @author    Дмитрий Васюков http://fictionlabs.ru
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * JBZoo Application
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Application
+ * @license    GPL-2.0
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @link       https://github.com/JBZoo/JBZoo
  */
 
-define('_JEXEC', 1);
+// no direct access
+defined('_JEXEC') or die('Restricted access');
 define( 'JPATH_BASE', realpath(dirname(__FILE__).'/../../../../../../..'));
 !defined('JBZOO_APP_GROUP') && define('JBZOO_APP_GROUP', 'jbuniversal');
 !defined('DIRECTORY_SEPERATOR') && define('DIRECTORY_SEPERATOR', '/');
